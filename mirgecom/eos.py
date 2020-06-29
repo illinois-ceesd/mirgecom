d69b3a3def140d6ee5bee95b15324db1bc6c03bd
Michael Campbell <mtcampbe@illinois.edu>
2020-06-29 14:35:22 -0500

    Improve docs on EOS

--- mirgecom/eos.py
+++ mirgecom/eos.py
@@ -29,8 +29,115 @@ import pyopencl.clmath as clmath
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 
 
+class GasEOS:
+    r"""Implements an object designed to provide methods
+    for implementing and computing relations between
+    fluid or gas state variables.
+    """
+
+    def __init__(self, parameters=None):
+        r"""Initialize routine should take any relevant
+        parameters expected to remain constant throughout
+        the simulation.
+        """
+        eos_is_implemented = False
+        assert eos_is_implemented
+
+    def __call__(self, w=None):
+        r"""Call to object
+
+        Parameters
+        ----------
+        w
+        State array which expects at least
+        the canonical conserved quantities:
+        :math:`w=[\rho,\rho{E},\rho\vec{V}]`
+        for the fluid at each point
+
+        Returns
+        ----------
+        object array of floating point arrays with the
+        EOS-specific dependent variables.
+        (e.g. [ pressure, temperature ] for an ideal gas)
+        """
+        assert False
+
+    def pressure(self, w=None):
+        r"""Gas pressure
+
+        Parameters
+        ----------
+        w
+        State array which expects at least
+        the canonical conserved quantities:
+        :math:`w=[\rho,\rho{E},\rho\vec{V}]`
+        for the fluid at each point
+
+        Returns
+        ----------
+        floating point array with the thermodynamic
+        pressure at each point
+        """
+        assert False
+
+    def temperature(self, w=None):
+        r"""Gas temperature
+
+        Parameters
+        ----------
+        w
+        State array which expects at least
+        the canonical conserved quantities:
+        :math:`w=[\rho,\rho{E},\rho\vec{V}]`
+        for the fluid at each point
+
+        Returns
+        ----------
+        floating point array with the thermodynamic
+        temperature at each point
+        """
+        assert False
+
+    def sound_speed(self, w=None):
+        r"""Speed of sound
+
+        Parameters
+        ----------
+        w
+        State array which expects at least
+        the canonical conserved quantities:
+        :math:`w=[\rho,\rho{E},\rho\vec{V}]`
+        for the fluid at each point
+
+        Returns
+        ----------
+        floating point number or array with the speed of sound
+        for each point
+        """
+        assert False
+
+    def gas_const(self, w=None):
+        r"""Specific gas constant (R)
+
+        Parameters
+        ----------
+        w
+        State array which expects at least
+        the canonical conserved quantities:
+        :math:`w=[\rho,\rho{E},\rho\vec{V}]`
+        for the fluid at each point
+
+        Returns
+        ----------
+        floating point number or array with the specific
+        gas constant for the domain, or at each point for
+        mixture-type EOS.
+        """
+        assert False
+
+
 class IdealSingleGas:
-    r""" Implements the ideal gas law (:math:`p = \rho{R}{T}`)
+    r"""Implements the ideal gas law (:math:`p = \rho{R}{T}`)
     for a single monatomic gas.
     """
 
@@ -67,9 +174,7 @@ class IdealSingleGas:
     def temperature(self, w):
         mass = w[0]
         temper = (
-            ((self._gamma - 1.0) / self._gas_const)
-            * self.internal_energy(w)
-            / mass
+            ((self._gamma - 1.0) / self._gas_const) * self.internal_energy(w) / mass
         )
         return temper
 
@@ -77,7 +182,9 @@ class IdealSingleGas:
         return flat_obj_array(self.pressure(w), self.temperature(w))
 
     def split_fields(self, ndim, dv):
-        retlist = [('pressure', dv[0]),
-                   ('temperature', dv[1]), ]
+        retlist = [
+            ("pressure", dv[0]),
+            ("temperature", dv[1]),
+        ]
 
         return retlist
