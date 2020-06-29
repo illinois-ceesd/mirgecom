161c2c8b1ba21a4065430329c67b96c4740e4178
Matthew Smith <mjsmith6@illinois.edu>
2020-06-29 11:10:23 -0500

    combine wave tests

--- test/test_wave.py
+++ test/test_wave.py
@@ -98,99 +98,56 @@ class EvaluationMapper(ev.EvaluationMapper):
             return clmath.cos(val)
 
 
-@pytest.mark.parametrize("dim", [2, 3])
-@pytest.mark.parametrize("order", [2, 3, 4])
-def test_standing_wave(ctx_factory, dim, order):
-    cl_ctx = ctx_factory()
-    queue = cl.CommandQueue(cl_ctx)
-
-    from pytools.convergence import EOCRecorder
-    eoc_rec = EOCRecorder()
-
-    for n in [4, 8, 16]:
+def get_standing_wave(dim):
+    # 2D: phi(x,y,t) = cos(sqrt(2)*c*t-pi/4)*cos(x)*cos(y)
+    # 3D: phi(x,y,z,t) = cos(sqrt(3)*c*t-pi/4)*cos(x)*cos(y)*cos(z)
+    # on [-pi/2, pi/2]^{#dims}
+    def mesh_factory(n):
         from meshmode.mesh.generation import generate_regular_rect_mesh
-        mesh = generate_regular_rect_mesh(
+        return generate_regular_rect_mesh(
                 a=(-0.5*np.pi,)*dim,
                 b=(0.5*np.pi,)*dim,
                 n=(n,)*dim)
-
-        from grudge.eager import EagerDGDiscretization
-        discr = EagerDGDiscretization(cl_ctx, mesh, order=order)
-
-        nodes = discr.nodes().with_queue(queue)
-
-        # 2D: phi(x,y,t) = cos(omega*t-pi/4)*cos(x)*cos(y)
-        # 3D: phi(x,y,z,t) = cos(omega*t-pi/4)*cos(x)*cos(y)*cos(z)
-        # where omega = sqrt(#dims)*c
-        coord_names = ["x", "y", "z"]
-        sym_coords = [pmbl.var(name) for name in coord_names]
-        sym_t = pmbl.var("t")
-        sym_c = pmbl.var("c")
-        sym_omega = pmbl.var("omega")
-        sym_pi = pmbl.var("pi")
-        sym_cos = pmbl.var("cos")
-        sym_phi = sym_cos(sym_omega*sym_t - sym_pi/4)
-        for i in range(dim):
-            sym_phi = sym_phi * sym_cos(sym_coords[i])
-
-        # u = phi_t
-        sym_u = sym_diff(sym_t)(sym_phi)
-
-        # v = c*grad(phi)
-        sym_v = [sym_c * sym_diff(sym_coords[i])(sym_phi) for i in range(dim)]
-
-        # rhs(u part) = c*div(v)
-        # rhs(v part) = c*grad(u)
-        sym_rhs = flat_obj_array(
-                    sym_c * sym_div(sym_v),
-                    make_obj_array([sym_c]) * sym_grad(dim, sym_u))
-
-        c = 2.
-
-        eval_values = {
-            "t": 0.,
-            "c": c,
-            "omega": np.sqrt(dim)*c,
-            "pi": np.pi
-        }
-        eval_values.update({coord_names[i]: nodes[i] for i in range(dim)})
-
-        def sym_eval(expr):
-            return EvaluationMapper(eval_values)(expr)
-
-        u = sym_eval(sym_u)
-        v = sym_eval(sym_v)
-        fields = flat_obj_array(u, v)
-
-        from mirgecom.wave import wave_operator
-        rhs = wave_operator(discr, c=c, w=fields)
-
-        expected_rhs = sym_eval(sym_rhs)
-
-        err = np.max(np.array([la.norm((rhs[i] - expected_rhs[i]).get(), np.inf)
-                for i in range(dim+1)]))
-        eoc_rec.add_data_point(1./n, err)
-
-        # from grudge.shortcuts import make_visualizer
-        # vis = make_visualizer(discr, discr.order)
-        # vis.write_vtk_file("result_{n}.vtu".format(n=n),
-        #         [
-        #             ("u", fields[0]),
-        #             ("v", fields[1:]),
-        #             ("rhs_u_actual", rhs[0]),
-        #             ("rhs_v_actual", rhs[1:]),
-        #             ("rhs_u_expected", expected_rhs[0]),
-        #             ("rhs_v_expected", expected_rhs[1:]),
-        #             ])
-
-    print("Approximation error:")
-    print(eoc_rec)
-    assert(eoc_rec.order_estimate() >= order - 0.5 or eoc_rec.max_error() < 1e-11)
+    sym_coords = [pmbl.var(name) for name in ["x", "y", "z"]]
+    sym_t = pmbl.var("t")
+    sym_c = pmbl.var("c")
+    sym_cos = pmbl.var("cos")
+    sym_phi = sym_cos(np.sqrt(dim)*sym_c*sym_t - np.pi/4)
+    for i in range(dim):
+        sym_phi = sym_phi * sym_cos(sym_coords[i])
+    return (dim, mesh_factory, sym_phi)
 
 
-@pytest.mark.parametrize("dim", [2, 3])
+def get_manufactured_cubic(dim):
+    # 2D: phi(x,y,t) = cos(t-pi/4)*(x-1)^3*(x+1)^3*(y-1)^3*(y+1)^3
+    # 3D: phi(x,y,z,t) = cos(t-pi/4)*(x-1)^3*(x+1)^3*(y-1)^3*(y+1)^3
+    #                      *(z-1)^3*(z+1)^3
+    # on [-1, 1]^{#dims}
+    # (Manufactured solution)
+    def mesh_factory(n):
+        from meshmode.mesh.generation import generate_regular_rect_mesh
+        return generate_regular_rect_mesh(
+                a=(-1.,)*dim,
+                b=(1.,)*dim,
+                n=(n,)*dim)
+    sym_coords = [pmbl.var(name) for name in ["x", "y", "z"]]
+    sym_t = pmbl.var("t")
+    sym_cos = pmbl.var("cos")
+    sym_phi = sym_cos(sym_t - np.pi/4)
+    for i in range(dim):
+        sym_phi = sym_phi * (sym_coords[i]-1)**3 * (sym_coords[i]+1)**3
+    return (dim, mesh_factory, sym_phi)
+
+
+@pytest.mark.parametrize(("dim", "mesh_factory", "sym_phi"),
+    [
+        get_standing_wave(2),
+        get_standing_wave(3),
+        get_manufactured_cubic(2),
+        get_manufactured_cubic(3)
+    ])
 @pytest.mark.parametrize("order", [2, 3, 4])
-def test_wave_manufactured(ctx_factory, dim, order):
+def test_wave(ctx_factory, dim, order, mesh_factory, sym_phi):
     cl_ctx = ctx_factory()
     queue = cl.CommandQueue(cl_ctx)
 
@@ -198,31 +155,20 @@ def test_wave_manufactured(ctx_factory, dim, order):
     eoc_rec = EOCRecorder()
 
     for n in [4, 8, 16]:
-        from meshmode.mesh.generation import generate_regular_rect_mesh
-        mesh = generate_regular_rect_mesh(
-                a=(-1.,)*dim,
-                b=(1.,)*dim,
-                n=(n,)*dim)
+        mesh = mesh_factory(n)
 
         from grudge.eager import EagerDGDiscretization
         discr = EagerDGDiscretization(cl_ctx, mesh, order=order)
 
         nodes = discr.nodes().with_queue(queue)
 
-        # 2D: phi(x,y,t) = cos(omega*t-pi/4)*(x-1)^3*(x+1)^3*(y-1)^3*(y+1)^3
-        # 3D: phi(x,y,z,t) = cos(omega*t-pi/4)*(x-1)^3*(x+1)^3*(y-1)^3*(y+1)^3
-        #                      *(z-1)^3*(z+1)^3
-        # Not actual solutions, but we can still apply wave operator to them
-        coord_names = ["x", "y", "z"]
-        sym_coords = [pmbl.var(name) for name in coord_names]
+        sym_coords = [pmbl.var(name) for name in ["x", "y", "z"]]
         sym_t = pmbl.var("t")
         sym_c = pmbl.var("c")
-        sym_omega = pmbl.var("omega")
-        sym_pi = pmbl.var("pi")
-        sym_cos = pmbl.var("cos")
-        sym_phi = sym_cos(sym_omega*sym_t - sym_pi/4)
-        for i in range(dim):
-            sym_phi = sym_phi * (sym_coords[i]-1)**3 * (sym_coords[i]+1)**3
+
+        # f = phi_tt - c^2 * div(grad(phi))
+        sym_f = sym_diff(sym_t)(sym_diff(sym_t)(sym_phi)) - sym_c**2\
+                    * sym_div(sym_grad(dim, sym_phi))
 
         # u = phi_t
         sym_u = sym_diff(sym_t)(sym_phi)
@@ -230,33 +176,32 @@ def test_wave_manufactured(ctx_factory, dim, order):
         # v = c*grad(phi)
         sym_v = [sym_c * sym_diff(sym_coords[i])(sym_phi) for i in range(dim)]
 
-        # rhs(u part) = c*div(v)
+        # rhs(u part) = c*div(v) + f
         # rhs(v part) = c*grad(u)
         sym_rhs = flat_obj_array(
-                    sym_c * sym_div(sym_v),
+                    sym_c * sym_div(sym_v) + sym_f,
                     make_obj_array([sym_c]) * sym_grad(dim, sym_u))
 
         c = 2.
 
-        eval_values = {
-            "t": 0.,
-            "c": c,
-            "omega": 1.,
-            "pi": np.pi
-        }
-        eval_values.update({coord_names[i]: nodes[i] for i in range(dim)})
-
-        def sym_eval(expr):
+        def sym_eval(expr, t):
+            eval_values = {
+                "t": t,
+                "c": c
+            }
+            coord_names = ["x", "y", "z"]
+            eval_values.update({coord_names[i]: nodes[i] for i in range(dim)})
             return EvaluationMapper(eval_values)(expr)
 
-        u = sym_eval(sym_u)
-        v = sym_eval(sym_v)
-        fields = flat_obj_array(u, v)
+        u = sym_eval(sym_u, 0.)
+        v = sym_eval(sym_v, 0.)
+        f = sym_eval(sym_f, 0.)
 
         from mirgecom.wave import wave_operator
-        rhs = wave_operator(discr, c=c, w=fields)
+        rhs = wave_operator(discr, c=c, w=flat_obj_array(u, v))
+        rhs[0] = rhs[0] + f
 
-        expected_rhs = sym_eval(sym_rhs)
+        expected_rhs = sym_eval(sym_rhs, 0.)
 
         err = np.max(np.array([la.norm((rhs[i] - expected_rhs[i]).get(), np.inf)
                 for i in range(dim+1)]))
@@ -266,8 +211,8 @@ def test_wave_manufactured(ctx_factory, dim, order):
         # vis = make_visualizer(discr, discr.order)
         # vis.write_vtk_file("result_{n}.vtu".format(n=n),
         #         [
-        #             ("u", fields[0]),
-        #             ("v", fields[1:]),
+        #             ("u", u),
+        #             ("v", v),
         #             ("rhs_u_actual", rhs[0]),
         #             ("rhs_v_actual", rhs[1:]),
         #             ("rhs_u_expected", expected_rhs[0]),
