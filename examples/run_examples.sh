f7cd9571bd7240d881992954f06c56ec3733f4c9
Michael Campbell <mtcampbe@illinois.edu>
2020-07-07 16:57:53 -0500

    Be a little more verbose and flexible about example running.

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 rm -f examples/*.vtu
-printf "Running examples...\n"
-for example in $(ls examples/*.py)
+examples_dir=${1}
+if [ -z ${examples_dir} ]
+then
+    examples_dir="."
+fi
+printf "Running examples in ${examples_dir}...\n"
+for example in $(ls ${examples_dir}/*.py)
 do
+    printf "Running example: ${example}\n"
     python ${example}
+    printf "Example ${example} "
+    if [ $? -eq 0 ]
+    then
+        printf "succeeded.\n"
+    else
+        printf "failed.\n"
+    fi
 done
-printf "done!\n" 
+printf "Done running examples!\n" 
 #rm -f examples/*.vtu
