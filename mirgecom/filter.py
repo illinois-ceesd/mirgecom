eacca150e4ee8971b066bcbfcc1066e5b572d309
Michael Campbell <mtcampbe@illinois.edu>
2020-07-14 11:45:07 -0500

    Add filter coeff util and test

---
+++ mirgecom/filter.py
@@ -0,0 +1,46 @@
+__copyright__ = """
+Copyright (C) 2020 University of Illinois Board of Trustees
+"""
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import math
+import numpy as np
+
+
+def get_spectral_filter(dim, order, cutoff, filter_order):
+    r"""
+    Exponential spectral filter from JSH/TW Nodal DG Methods, pp. 130, 186
+    """
+    npol = 1
+    for d in range(1, dim+1):
+        npol *= (order + d)
+    npol /= math.factorial(int(dim))
+    npol = int(npol)
+    filter = np.identity(npol)
+    alpha = -1.0*np.log(np.finfo(float).eps)
+    nfilt = npol - cutoff
+    if nfilt <= 0:
+        return filter
+    nstart = cutoff - 1
+    for m in range(nstart, npol):
+        filter[m, m] = np.exp(-1.0 * alpha
+                              * ((m - nstart) / nfilt) ** filter_order)
+    return filter
--- test/test_filter.py
+++ test/test_filter.py
@@ -21,48 +21,63 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-import numpy as np
-import logging
+import math
 import pytest
-#import pyopencl as cl
+import numpy as np
 
-#from pytools.obj_array import (
-#    flat_obj_array,
-#    make_obj_array,
-#)
-from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import EagerDGDiscretization
 from pyopencl.tools import (  # noqa
     pytest_generate_tests_for_pyopencl as pytest_generate_tests,
 )
+from mirgecom.filter import get_spectral_filter
 
 
+@pytest.mark.parametrize("cutoff", [3, 4, 8])
 @pytest.mark.parametrize("dim", [1, 2, 3])
-@pytest.mark.parametrize("order", [1, 2, 3])
-def test_vandermonde(ctx_factory, dim, order):
+@pytest.mark.parametrize("order", [1, 2, 3, 4])
+def test_filter_coeff(ctx_factory, cutoff, dim, order):
+    """
+    Tests that the filter coefficients have the right shape
+    and a quick sanity check on the values at the filter
+    band limits.
+    """
     cl_ctx = ctx_factory()
-    #    queue = cl.CommandQueue(cl_ctx)
-    logger = logging.getLogger(__name__)
     nel_1d = 16
 
     from meshmode.mesh.generation import generate_regular_rect_mesh
 
-    #    for dim in [1, 2, 3]:
     mesh = generate_regular_rect_mesh(
         a=(-0.5,) * dim, b=(0.5,) * dim, n=(nel_1d,) * dim
     )
 
     discr = EagerDGDiscretization(cl_ctx, mesh, order=order)
     vol_discr = discr.discr_from_dd("vol")
-    ngroups = len(vol_discr.groups)
-    logger.info(f'Number of groups: {ngroups}')
+
+    # number of polynomials
+    numpoly = 1
+    for d in range(1, dim+1):
+        numpoly *= (order + d)
+    numpoly /= math.factorial(int(dim))
+    numpoly = int(numpoly)
+
+    # number of filtered modes
+    nfilt = numpoly - cutoff
+    # alpha = f(machine eps)
+    alpha = -1.0*np.log(np.finfo(float).eps)
+
+    # expected values @ filter band limits
+    expected_high_coeff = np.exp(-1.0*alpha)
+    low_index = cutoff - 1
+    high_index = numpoly - 1
+    if nfilt <= 0:
+        expected_high_coeff = 1.0
+        low_index = 0
 
     from modepy import vandermonde
     for group in vol_discr.groups:
         vander = vandermonde(group.basis(), group.unit_nodes)
         vanderm1 = np.linalg.inv(vander)
-        imaybe = np.matmul(vander, vanderm1)
-        iresid = np.abs(imaybe - np.identity(len(group.unit_nodes[0])))
-        maxerr = np.max(iresid)
-        assert(maxerr <= 1e-15)
-
+        filter_coeff = get_spectral_filter(dim, order, cutoff, 2)
+        assert(filter_coeff.shape == vanderm1.shape)
+        assert(filter_coeff[high_index][high_index] == expected_high_coeff)
+        assert(filter_coeff[low_index][low_index] == 1.0)
