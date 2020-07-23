ca57759839076d2530f27c4cb08ebb67b8c1fc79
Matthias Diener <mdiener@illinois.edu>
2020-07-23 14:53:46 -0500

    make mpi4py required again

--- mirgecom/__init__.py
+++ mirgecom/__init__.py
@@ -25,26 +25,23 @@ import getpass
 import sys
 from warnings import warn
 
-try:
-    from mpi4py import MPI
+from mpi4py import MPI
 
-    # This code avoids slow startups due to file system locking when running with
-    # large numbers of ranks. See https://github.com/illinois-ceesd/planning/issues/27
-    # for details.
+# This code avoids slow startups due to file system locking when running with
+# large numbers of ranks. See https://github.com/illinois-ceesd/planning/issues/27
+# for details.
 
-    size = MPI.COMM_WORLD.Get_size()
+size = MPI.COMM_WORLD.Get_size()
 
-    if size >= 128:
-        for mod in ["pyopencl", "pytools", "loopy"]:
-            if mod in sys.modules:
-                warn("{} already loaded, not adjusting XDG_CACHE_HOME.".format(mod))
+if size >= 128:
+    for mod in ["pyopencl", "pytools", "loopy"]:
+        if mod in sys.modules:
+            warn("{} already loaded, not adjusting XDG_CACHE_HOME.".format(mod))
 
-        if "XDG_CACHE_HOME" in os.environ:
-            warn("XDG_CACHE_HOME already set, not adjusting it.")
-        else:
-            rank = MPI.COMM_WORLD.Get_rank()
-            username = getpass.getuser()
+    if "XDG_CACHE_HOME" in os.environ:
+        warn("XDG_CACHE_HOME already set, not adjusting it.")
+    else:
+        rank = MPI.COMM_WORLD.Get_rank()
+        username = getpass.getuser()
 
-            os.environ["XDG_CACHE_HOME"] = "/tmp/{}/xdg-cache-r{}".format(username, rank)
-except:
-    pass
+        os.environ["XDG_CACHE_HOME"] = "/tmp/{}/xdg-cache-r{}".format(username, rank)
