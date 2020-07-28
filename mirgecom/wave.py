10cdc4448f518828084845c246582aebda16f90a
Michael Campbell <mtcampbe@illinois.edu>
2020-07-28 00:11:05 -0500

    Port to array context

--- mirgecom/wave.py
+++ mirgecom/wave.py
@@ -23,11 +23,11 @@ THE SOFTWARE.
 import numpy as np
 import numpy.linalg as la  # noqa
 from pytools.obj_array import (
-        flat_obj_array, make_obj_array,
-        with_object_array_or_scalar)
+    flat_obj_array, make_obj_array)
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
-from grudge.eager import with_queue
+from meshmode.dof_array import thaw
 from grudge.symbolic.primitives import TracePair
+from grudge.eager import interior_trace_pair
 
 
 __doc__ = """
@@ -35,58 +35,61 @@ __doc__ = """
 """
 
 
-def _interior_trace_pair(discr, vec):
-    i = discr.interp("vol", "int_faces", vec)
-    e = with_object_array_or_scalar(
-            lambda el: discr.opposite_face_connection()(el.queue, el),
-            i)
-    return TracePair("int_faces", i, e)
-
-
 def _flux(discr, c, w_tpair):
     u = w_tpair[0]
     v = w_tpair[1:]
 
-    normal = with_queue(u.int.queue, discr.normal(w_tpair.dd))
+    actx = w_tpair.int[0].array_context
+
+    normal = thaw(actx, discr.normal(w_tpair.dd))
 
     def normal_times(scalar):
         # workaround for object array behavior
         return make_obj_array([ni*scalar for ni in normal])
 
     flux_weak = flat_obj_array(
-            np.dot(v.avg, normal),
-            normal_times(u.avg),
-            )
+        np.dot(v.avg, normal),
+        normal_times(u.avg),
+        )
 
     # upwind
     v_jump = np.dot(normal, v.int-v.ext)
     flux_weak += flat_obj_array(
-            0.5*(u.int-u.ext),
-            0.5*normal_times(v_jump),
-            )
+        0.5*(u.int-u.ext),
+        0.5*normal_times(v_jump),
+        )
 
-    return discr.interp(w_tpair.dd, "all_faces", c*flux_weak)
+    return discr.project(w_tpair.dd, "all_faces", c*flux_weak)
 
 
 def wave_operator(discr, c, w):
+    """
+    Args:
+        discr (grudge.eager.EagerDGDiscretization): the discretization to use
+        c (float): the (constant) wave speed)
+        w (np.ndarray): an object array of DOF arrays, representing the state vector
+
+    Returns:
+        np.ndarray: an object array of DOF arrays, representing the ODE RHS
+    """
     u = w[0]
     v = w[1:]
 
-    dir_u = discr.interp("vol", BTAG_ALL, u)
-    dir_v = discr.interp("vol", BTAG_ALL, v)
+    dir_u = discr.project("vol", BTAG_ALL, u)
+    dir_v = discr.project("vol", BTAG_ALL, v)
     dir_bval = flat_obj_array(dir_u, dir_v)
     dir_bc = flat_obj_array(-dir_u, dir_v)
 
     return (
-            discr.inverse_mass(
-                flat_obj_array(
-                    -c*discr.weak_div(v),
-                    -c*discr.weak_grad(u)
-                    )
-                +  # noqa: W504
-                discr.face_mass(
-                    _flux(discr, c=c, w_tpair=_interior_trace_pair(discr, w))
-                    + _flux(discr, c=c, w_tpair=TracePair(BTAG_ALL, dir_bval,
-                    dir_bc))
-                    ))
+        discr.inverse_mass(
+            flat_obj_array(
+                -c*discr.weak_div(v),
+                -c*discr.weak_grad(u)
                 )
+            +  # noqa: W504
+            discr.face_mass(
+                _flux(discr, c=c, w_tpair=interior_trace_pair(discr, w))
+                + _flux(discr, c=c, w_tpair=TracePair(BTAG_ALL, dir_bval,
+                dir_bc))
+                ))
+        )
