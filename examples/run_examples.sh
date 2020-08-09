9ed242250856f010237da5a20e4bfe0969cc25ad
Mike Campbell <mtcampbe@illinois.edu>
2020-08-09 16:42:52 -0500

    Add example runner script (#55)
    * Add example runner script
    
    * Rearrange to get the correct exit code from the example run.
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Diener <mdiener@illinois.edu>
    
    * Replace printf with echo, [ with [[
    
    * Exercise the run_examples in testing
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Diener <mdiener@illinois.edu>
    
    * Strictify the detection of mpi examples
    
    Co-authored-by: Matthias Diener <mdiener@illinois.edu>

---
+++ examples/run_examples.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# set -e
+set -o nounset
+
+origin=$(pwd)
+examples_dir=${1-$origin}
+declare -i exitcode=0
+echo "Running examples in $examples_dir ..."
+for example in $examples_dir/*.py
+do
+    if [[ "$example" == *"-mpi.py" ]]
+    then
+        echo "Running parallel example: $example"        
+        mpiexec -n 2 python ${example}
+    else
+        echo "Running serial example: $example"        
+        python ${example}
+    fi
+    if [[ $? -eq 0 ]]
+    then
+        echo "Example $example succeeded."
+    else
+        ((exitcode=exitcode+1))
+        echo "Example $example failed."
+    fi
+done
+echo "Done running examples!"
+if [[ $exitcode -eq 0 ]]
+then
+    echo "No errors."
+else
+    echo "Errors detected ($exitcode)."
+    exit $exitcode
+fi
+#rm -f examples/*.vtu
