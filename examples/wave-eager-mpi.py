e448057b340c2f489408377a19b09b1d0859fad4
Matthias Diener <mdiener@illinois.edu>
2020-08-11 16:06:57 -0500

    Update examples/wave-eager-mpi.py
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>

--- examples/wave-eager-mpi.py
+++ examples/wave-eager-mpi.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright (C) 2020 Andreas Kloeckner"
+__copyright__ = "Copyright (C) 2020 University of Illinois Board of Trustees"
 
 __license__ = """
 Permission is hereby granted, free of charge, to any person obtaining a copy
