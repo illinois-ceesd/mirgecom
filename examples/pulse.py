2b5daafab2ceed294094d0c58d99b44bf94cda25
Michael Campbell <mtcampbe@illinois.edu>
2020-08-14 06:54:10 -0500

    Debug the pulse case

--- examples/pulse.py
+++ examples/pulse.py
@@ -34,23 +34,97 @@ from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import EagerDGDiscretization
 from grudge.shortcuts import make_visualizer
 
-
-from mirgecom.euler import inviscid_operator
+from mirgecom.euler import (
+    inviscid_operator,
+    split_conserved
+)
 from mirgecom.simutil import (
     inviscid_sim_timestep,
-    sim_checkpoint
+    #    sim_checkpoint
 )
 from mirgecom.io import make_init_message
 
 from mirgecom.integrators import rk4_step
 from mirgecom.steppers import advance_state
-from mirgecom.boundary import PrescribedBoundary
-from mirgecom.initializers import Lump
+from mirgecom.boundary import DummyBoundary
+from mirgecom.initializers import make_pulse
 from mirgecom.eos import IdealSingleGas
 from pytools.obj_array import (
     flat_obj_array,
     make_obj_array,
 )
+from mirgecom.io import (
+    make_io_fields,
+    make_status_message,
+    make_output_dump,
+)
+from mirgecom.checkstate import compare_states
+# from mirgecom.euler import (
+#     get_inviscid_timestep,
+# )
+from mirgecom.simutil import (
+    #      inviscid_sim_timestep,
+    check_step,
+    #    make_status_message,
+)
+
+
+def sim_checkpoint(discr, visualizer, eos, logger, q, vizname, exact_soln=None,
+                   step=0, t=0, dt=0, cfl=1.0, nstatus=-1, nviz=-1, exittol=1e-16,
+                   constant_cfl=False, comm=None):
+    r"""
+    Checkpointing utility for runs with known exact solution generator
+    """
+
+    do_viz = check_step(step=step, interval=nviz)
+    do_status = check_step(step=step, interval=nstatus)
+    if do_viz is False and do_status is False:
+        return 0
+
+    actx = q[0].array_context
+    nodes = thaw(actx, discr.nodes())
+    rank = 0
+
+    if comm is not None:
+        rank = comm.Get_rank()
+    checkpoint_status = 0
+
+    dv = eos(q=q)
+
+    have_exact = False
+
+    if ((do_status is True or do_viz is True) and exact_soln is not None):
+        have_exact = True
+        expected_state = exact_soln(t=t, x_vec=nodes, eos=eos)
+
+    if do_status is True:
+        #        if constant_cfl is False:
+        #            current_cfl = get_inviscid_cfl(discr=discr, q=q,
+        #                                           eos=eos, dt=dt)
+        statusmesg = make_status_message(t=t, step=step, dt=dt,
+                                         cfl=cfl, dv=dv)
+        if have_exact is True:
+            max_errors = compare_states(red_state=q, blue_state=expected_state)
+            statusmesg += f"\n------   Err({max_errors})"
+            if rank == 0:
+                logger.info(statusmesg)
+
+            maxerr = np.max(max_errors)
+            if maxerr > exittol:
+                logger.error("Solution failed to follow expected result.")
+                checkpoint_status = 1
+
+    if do_viz:
+        dim = discr.dim
+        io_fields = make_io_fields(dim, q, dv, eos)
+        if have_exact is True:
+            io_fields.append(("exact_soln", expected_state))
+            result_resid = q - expected_state
+            io_fields.append(("residual", result_resid))
+        make_output_dump(visualizer, basename=vizname, io_fields=io_fields,
+                         comm=comm, step=step, t=t, overwrite=True)
+
+    return checkpoint_status
 
 
 # Surrogate for the currently non-functioning Uniform class
@@ -92,13 +166,12 @@ def main(ctx_factory=cl.create_some_context):
     current_cfl = 1.0
     vel = np.zeros(shape=(dim,))
     orig = np.zeros(shape=(dim,))
-    vel[:dim] = 1.0
     current_dt = .001
     current_t = 0
     eos = IdealSingleGas()
-    initializer = Lump(center=orig, velocity=vel)
-    casename = 'lump'
-    boundaries = {BTAG_ALL: PrescribedBoundary(initializer)}
+    #    initializer = Lump(center=orig, velocity=vel)
+    casename = 'pulse'
+    boundaries = {BTAG_ALL: DummyBoundary}
     constant_cfl = False
     nstatus = 10
     nviz = 10
@@ -110,16 +183,20 @@ def main(ctx_factory=cl.create_some_context):
     from meshmode.mesh.generation import generate_regular_rect_mesh
 
     mesh = generate_regular_rect_mesh(
-        a=(-5.0,) * dim, b=(5.0,) * dim, n=(nel_1d,) * dim
+        a=(-.5,) * dim, b=(.5,) * dim, n=(nel_1d,) * dim
     )
 
     discr = EagerDGDiscretization(actx, mesh, order=order)
     nodes = thaw(actx, discr.nodes())
-    current_state = initializer(0, nodes)
+    q = set_uniform_solution(t=0, x_vec=nodes, eos=eos)
+    qs = split_conserved(dim, q)
+    r0 = np.zeros(dim)
+    qs.energy = qs.energy + make_pulse(amp=1.0, w=.1, r0=r0, r=nodes)
+    current_state = q
 
     visualizer = make_visualizer(discr, discr.order + 3
                                  if discr.dim == 2 else discr.order)
-    initname = initializer.__class__.__name__
+    initname = "pulse"
     eosname = eos.__class__.__name__
     init_message = make_init_message(dim=dim, order=order, nelements=mesh.nelements,
                                      dt=current_dt, t_final=t_final, nstatus=nstatus,
@@ -138,7 +215,7 @@ def main(ctx_factory=cl.create_some_context):
                                  boundaries=boundaries, eos=eos)
 
     def my_checkpoint(step, t, dt, state):
-        return sim_checkpoint(discr, initializer, visualizer, eos, logger,
+        return sim_checkpoint(discr, visualizer, eos, logger,
                               q=state, vizname=casename, step=step, t=t, dt=dt,
                               nstatus=nstatus, nviz=nviz, exittol=exittol,
                               constant_cfl=constant_cfl)
@@ -151,9 +228,10 @@ def main(ctx_factory=cl.create_some_context):
     #    if current_t != checkpoint_t:
     if rank == 0:
         logger.info("Checkpointing final state ...")
-        my_checkpoint(current_step, t=current_t,
-                      dt=(current_t - checkpoint_t),
-                      state=current_state)
+
+    my_checkpoint(current_step, t=current_t,
+                  dt=(current_t - checkpoint_t),
+                  state=current_state)
 
     if current_t - t_final < 0:
         raise ValueError("Simulation exited abnormally")
