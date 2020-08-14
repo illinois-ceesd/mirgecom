069be3d9def9becb7b4a29d980873c4f90eef40a
Michael Campbell <mtcampbe@illinois.edu>
2020-08-14 06:28:53 -0500

    Add pulse example

---
+++ examples/pulse.py
@@ -0,0 +1,166 @@
+__copyright__ = """
+Copyright (C) 2020 University of Illinois Board of Trustees
+"""
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import logging
+import numpy as np
+import pyopencl as cl
+import numpy.linalg as la  # noqa
+import pyopencl.array as cla  # noqa
+from functools import partial
+
+from meshmode.array_context import PyOpenCLArrayContext
+from meshmode.dof_array import thaw
+from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
+from grudge.eager import EagerDGDiscretization
+from grudge.shortcuts import make_visualizer
+
+
+from mirgecom.euler import inviscid_operator
+from mirgecom.simutil import (
+    inviscid_sim_timestep,
+    sim_checkpoint
+)
+from mirgecom.io import make_init_message
+
+from mirgecom.integrators import rk4_step
+from mirgecom.steppers import advance_state
+from mirgecom.boundary import PrescribedBoundary
+from mirgecom.initializers import Lump
+from mirgecom.eos import IdealSingleGas
+from pytools.obj_array import (
+    flat_obj_array,
+    make_obj_array,
+)
+
+
+# Surrogate for the currently non-functioning Uniform class
+def set_uniform_solution(t, x_vec, eos=IdealSingleGas()):
+
+    dim = len(x_vec)
+    _rho = 1.0
+    _p = 1.0
+    _velocity = np.zeros(shape=(dim,))
+    _gamma = 1.4
+
+    mom0 = _rho * _velocity
+    e0 = _p / (_gamma - 1.0)
+    ke = 0.5 * np.dot(_velocity, _velocity) / _rho
+    x_rel = x_vec[0]
+    zeros = 0.0*x_rel
+    ones = zeros + 1.0
+
+    mass = zeros + _rho
+    mom = make_obj_array([mom0 * ones for i in range(dim)])
+    energy = e0 + ke + zeros
+
+    return flat_obj_array(mass, energy, mom)
+
+
+def main(ctx_factory=cl.create_some_context):
+
+    cl_ctx = ctx_factory()
+    queue = cl.CommandQueue(cl_ctx)
+    actx = PyOpenCLArrayContext(queue)
+
+    logger = logging.getLogger(__name__)
+
+    dim = 2
+    nel_1d = 16
+    order = 3
+    exittol = 2e-2
+    t_final = 0.1
+    current_cfl = 1.0
+    vel = np.zeros(shape=(dim,))
+    orig = np.zeros(shape=(dim,))
+    vel[:dim] = 1.0
+    current_dt = .001
+    current_t = 0
+    eos = IdealSingleGas()
+    initializer = Lump(center=orig, velocity=vel)
+    casename = 'lump'
+    boundaries = {BTAG_ALL: PrescribedBoundary(initializer)}
+    constant_cfl = False
+    nstatus = 10
+    nviz = 10
+    rank = 0
+    checkpoint_t = current_t
+    current_step = 0
+    timestepper = rk4_step
+
+    from meshmode.mesh.generation import generate_regular_rect_mesh
+
+    mesh = generate_regular_rect_mesh(
+        a=(-5.0,) * dim, b=(5.0,) * dim, n=(nel_1d,) * dim
+    )
+
+    discr = EagerDGDiscretization(actx, mesh, order=order)
+    nodes = thaw(actx, discr.nodes())
+    current_state = initializer(0, nodes)
+
+    visualizer = make_visualizer(discr, discr.order + 3
+                                 if discr.dim == 2 else discr.order)
+    initname = initializer.__class__.__name__
+    eosname = eos.__class__.__name__
+    init_message = make_init_message(dim=dim, order=order, nelements=mesh.nelements,
+                                     dt=current_dt, t_final=t_final, nstatus=nstatus,
+                                     nviz=nviz, cfl=current_cfl,
+                                     constant_cfl=constant_cfl, initname=initname,
+                                     eosname=eosname, casename=casename)
+    if rank == 0:
+        logger.info(init_message)
+
+    get_timestep = partial(inviscid_sim_timestep, discr=discr, t=current_t,
+                           dt=current_dt, cfl=current_cfl, eos=eos,
+                           t_final=t_final, constant_cfl=constant_cfl)
+
+    def my_rhs(t, state):
+        return inviscid_operator(discr, q=state, t=t,
+                                 boundaries=boundaries, eos=eos)
+
+    def my_checkpoint(step, t, dt, state):
+        return sim_checkpoint(discr, initializer, visualizer, eos, logger,
+                              q=state, vizname=casename, step=step, t=t, dt=dt,
+                              nstatus=nstatus, nviz=nviz, exittol=exittol,
+                              constant_cfl=constant_cfl)
+
+    (current_step, current_t, current_state) = \
+        advance_state(rhs=my_rhs, timestepper=timestepper, checkpoint=my_checkpoint,
+                    get_timestep=get_timestep, state=current_state,
+                    t=current_t, t_final=t_final)
+
+    #    if current_t != checkpoint_t:
+    if rank == 0:
+        logger.info("Checkpointing final state ...")
+        my_checkpoint(current_step, t=current_t,
+                      dt=(current_t - checkpoint_t),
+                      state=current_state)
+
+    if current_t - t_final < 0:
+        raise ValueError("Simulation exited abnormally")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    main()
+
+# vim: foldmethod=marker
