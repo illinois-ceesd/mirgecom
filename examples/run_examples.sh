60b492018e54aede79b1a55d7f8bb27870741211
Michael T. Campbell <mtcampbe@illinois.edu>
2020-08-22 07:06:43 -0700

    Switch back to mpiexec

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -12,7 +12,7 @@ do
     if [[ "$example" == *"-mpi.py" ]]
     then
         echo "Running parallel example: $example"        
-        srun -n 2 python ${example}
+        mpiexec -n 2 python ${example}
     else
         echo "Running serial example: $example"        
         python ${example}
