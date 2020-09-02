75ade9d90b9c8ea5711161fde313559108f31653
Michael T. Campbell <mtcampbe@illinois.edu>
2020-09-02 10:39:27 -0700

    Add adiabatic slip boundary implementation

--- mirgecom/boundary.py
+++ mirgecom/boundary.py
@@ -22,10 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import numpy as np
 from meshmode.dof_array import thaw
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from mirgecom.eos import IdealSingleGas
 from grudge.symbolic.primitives import TracePair
+from mirgecom.euler import split_conserved
 
 
 __doc__ = """
@@ -38,7 +40,8 @@ Boundary Conditions
 
 
 class PrescribedBoundary:
-    """Boundary condition assigns the boundary solution with a
+    """
+    Boundary condition assigns the boundary solution with a
     user-specified function
 
     .. automethod:: __init__
@@ -60,7 +63,8 @@ class PrescribedBoundary:
 
 
 class DummyBoundary:
-    """Simple example boundary condition that interpolates the
+    """
+    Simple example boundary condition that interpolates the
     boundary-adjacent volume solution to both sides of a boundary
     face.
 
@@ -71,3 +75,59 @@ class DummyBoundary:
     ):
         dir_soln = discr.project("vol", btag, q)
         return TracePair(btag, interior=dir_soln, exterior=dir_soln)
+
+
+class AdiabaticSlipBoundary:
+    """
+    Adiabatic slipwall boundary for inviscid flows
+
+    .. automethod:: boundary_pair
+    """
+    def boundary_pair(
+            self, discr, q, t=0.0, btag=BTAG_ALL, eos=IdealSingleGas()
+    ):
+        # Grab some boundary-relevant data
+        dim = discr.dim
+        cv = split_conserved(dim, q)
+        actx = cv.mass.array_context
+
+        normal = thaw(actx, discr.normal(btag))
+        normal_mag = actx.np.sqrt(np.dot(normal, normal))
+        nhat_mult = 1.0 / normal_mag
+        #        print(f"normal={normal}")
+        #        print(f"normal_mag={normal_mag}")
+        # nhat = normal * make_obj_array(nhat_mult)
+        nhat = normal
+        for i in range(dim):
+            nhat[i] = nhat_mult * nhat[i]
+
+        # Get the interior/exterior solns
+        int_soln = discr.project("vol", btag, q)
+        boundary_soln = discr.project("vol", btag, q)  # copy?
+        bpressure = eos.pressure(boundary_soln)
+        #        bsoln = split_conserved(dim, boundary_soln)
+
+        # Subtract out the wall-normal component
+        # of velocity from the velocity at the wall
+        #        wall_velocity = bsoln.momentum / bsoln.mass
+        wall_velocity = 1.0 * boundary_soln[2:]
+        wnorm_vel = 1.0 * wall_velocity
+        for i in range(dim):
+            wall_velocity[i] = wall_velocity[i] / boundary_soln[0]
+        nvelhat = np.dot(wall_velocity, nhat)
+        for i in range(dim):
+            wnorm_vel[i] = nvelhat * normal[i]
+            wall_velocity[i] = wall_velocity[i] - 2.0 * wnorm_vel[i]
+
+        # wall_velocity = wall_velocity - 2.0 * wnorm_vel
+
+        # Re-calculate the boundary solution with the new
+        # momentum
+
+        #        bsoln.momentum = bsoln.mass * wall_velocity
+        #        bsoln.energy = eos.energy(bsoln.mass, bpressure, bsoln.momentum)
+        for i in range(dim):
+            boundary_soln[2 + i] = boundary_soln[0] * wall_velocity[i]
+        boundary_soln[1] = eos.energy(boundary_soln[0], bpressure, boundary_soln[2:])
+
+        return TracePair(btag, int_soln, boundary_soln)
