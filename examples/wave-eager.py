9307a331497e606ff7d648d6b9ae90a5d5f70f59
Matthias Diener <mdiener@illinois.edu>
2020-09-04 11:27:21 -0500

    add help

--- examples/wave-eager.py
+++ examples/wave-eager.py
@@ -123,7 +123,8 @@ def main(use_profiling=False):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Wave-eager (non-MPI version)')
-    parser.add_argument('--profile', action='store_true')
+    parser.add_argument('--profile', action='store_true',
+        help='enable kernel profiling')
     args = parser.parse_args()
 
     main(use_profiling=args.profile)
