53d0098284f9812ca5bb61ba319d9135dbe98250
Matthias Diener <mdiener@illinois.edu>
2020-10-25 17:32:24 -0500

    add to vortex-mpi

--- examples/vortex-mpi.py
+++ examples/vortex-mpi.py
@@ -33,6 +33,7 @@ from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import EagerDGDiscretization
 from grudge.shortcuts import make_visualizer
 
+from mirgecom.profiling import PyOpenCLProfilingArrayContext
 
 from mirgecom.euler import inviscid_operator
 from mirgecom.simutil import (
@@ -50,17 +51,36 @@ from mirgecom.boundary import PrescribedBoundary
 from mirgecom.initializers import Vortex2D
 from mirgecom.eos import IdealSingleGas
 
+from logpyle import (LogManager, add_general_quantities,
+        add_simulation_quantities, add_run_info)
+
+from mirgecom.logging_quantities import PhysicalQuantity, KernelProfile
+
 
 logger = logging.getLogger(__name__)
 
 
 @mpi_entry_point
-def main(ctx_factory=cl.create_some_context):
+def main(ctx_factory=cl.create_some_context, use_profiling=False, use_logmgr=False):
+
+    if use_logmgr:
+        logmgr = LogManager("vortex.dat", "wu")  # , comm=...
+        add_run_info(logmgr)
+        add_general_quantities(logmgr)
+        add_simulation_quantities(logmgr)
+    else:
+        logmgr = None
 
     cl_ctx = ctx_factory()
-    queue = cl.CommandQueue(cl_ctx)
-    actx = PyOpenCLArrayContext(queue,
-                allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
+    if use_profiling:
+        queue = cl.CommandQueue(cl_ctx,
+            properties=cl.command_queue_properties.PROFILING_ENABLE)
+        actx = PyOpenCLProfilingArrayContext(queue,
+            allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
+    else:
+        queue = cl.CommandQueue(cl_ctx)
+        actx = PyOpenCLArrayContext(queue,
+            allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
 
     dim = 2
     nel_1d = 16
@@ -103,6 +123,16 @@ def main(ctx_factory=cl.create_some_context):
     nodes = thaw(actx, discr.nodes())
     current_state = initializer(0, nodes)
 
+    if use_logmgr:
+        logmgr.add_quantity(PhysicalQuantity(discr, eos, "pressure", "P", "min"))
+        logmgr.add_quantity(PhysicalQuantity(discr, eos, "temperature", "K", "min"))
+        if rank == 0:
+            logmgr.add_watches(["step", "t_step", "min_pressure", "min_temperature"])
+        if use_profiling:
+            logmgr.add_quantity(KernelProfile(actx, "diff", "flops"))
+            if rank == 0:
+                logmgr.add_watches(["diff_flops"])
+
     visualizer = make_visualizer(discr, discr.order + 3
                                  if discr.dim == 2 else discr.order)
     initname = initializer.__class__.__name__
@@ -136,7 +166,7 @@ def main(ctx_factory=cl.create_some_context):
             advance_state(rhs=my_rhs, timestepper=timestepper,
                           checkpoint=my_checkpoint,
                           get_timestep=get_timestep, state=current_state,
-                          t=current_t, t_final=t_final)
+                          t=current_t, t_final=t_final, logmgr=logmgr)
     except ExactSolutionMismatch as ex:
         current_step = ex.step
         current_t = ex.t
@@ -152,9 +182,22 @@ def main(ctx_factory=cl.create_some_context):
     if current_t - t_final < 0:
         raise ValueError("Simulation exited abnormally")
 
+    if use_logmgr:
+        logmgr.close()
+    elif use_profiling:
+        print(actx.tabulate_profiling_data())
+
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(message)s", level=logging.INFO)
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="Sod (MPI version)")
+    parser.add_argument("--profile", action="store_true",
+        help="enable kernel profiling")
+    parser.add_argument("--logging", action="store_true",
+        help="enable time series logging")
+    args = parser.parse_args()
+
+    main(use_profiling=args.profile, use_logmgr=args.logging)
 
 # vim: foldmethod=marker
