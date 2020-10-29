b7a7ce6707fafc6ab36ef6936ad6b5262078ddf6
Michael T. Campbell <mtcampbe@illinois.edu>
2020-10-28 17:01:12 -0700

    Fix broken ref

--- mirgecom/euler.py
+++ mirgecom/euler.py
@@ -204,7 +204,7 @@ def _facial_flux(discr, eos, q_tpair, local=False):
     flux_int = inviscid_flux(discr, eos, q_tpair.int)
     flux_ext = inviscid_flux(discr, eos, q_tpair.ext)
 
-    # Lax-Friedrichs/Rusanov after [DGBook]_, Section 6.6
+    # Lax-Friedrichs/Rusanov after [Hesthaven_2008]_, Section 6.6
     flux_avg = 0.5*(flux_int + flux_ext)
 
     lam = actx.np.maximum(
