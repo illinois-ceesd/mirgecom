598e4b6742210628a7a43a27c6473b02de3783ec
Wyatt Hagen <wahagen2@illinois.edu>
2020-11-06 15:33:28 -0600

    Fixed updated artificial viscosity implementation to use desired equations

--- mirgecom/artificial_viscosity.py
+++ mirgecom/artificial_viscosity.py
@@ -28,7 +28,7 @@ THE SOFTWARE.
 #from dataclasses import dataclass
 
 import numpy as np
-from pytools.obj_array import make_obj_array
+from pytools.obj_array import make_obj_array, obj_array_vectorize, flat_obj_array,obj_array_vectorize_n_args
 from meshmode.dof_array import thaw
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import (
@@ -39,86 +39,145 @@ from grudge.symbolic.primitives import TracePair
 from mirgecom.euler import split_conserved, join_conserved
 
 
-def scalar(s):
-    """Create an object array for a scalar."""
-    return make_obj_array([s])
-
-
-def dissapative_flux(discr, q):
+def _facial_flux_r(discr, q_tpair):
 
     dim = discr.dim
-    cv = split_conserved(dim, q)
+    actx = q_tpair[0].int.array_context
 
-    return join_conserved(dim,
-            mass=np.ones(dim)*scalar(cv.mass),
-            energy=np.ones(dim)*scalar(cv.energy),
-            momentum= np.ones((dim,dim))*cv.momentum )
+    flux_dis = q_tpair.avg
 
-def _facial_flux(discr, q_tpair):
+    normal = thaw(actx, discr.normal(q_tpair.dd))
+
+    flux_out = flux_dis * normal
+    
+    # Can't do it here... "obj arrays not allowed on compute device"
+    #def flux_calc(flux):
+    #    return (flux * normal)
+    #flux_out = obj_array_vectorize(flux_calc,flux_dis)
+
+    return discr.project(q_tpair.dd, "all_faces", flux_out)
+
+def _facial_flux_q(discr, q_tpair):
 
     dim = discr.dim
 
     actx = q_tpair[0].int.array_context
 
-    flux_int = dissapative_flux(discr,q_tpair.int);
-    flux_ext = dissapative_flux(discr,q_tpair.ext);
-    flux_dis = 0.5*(flux_ext + flux_int); 
-
     normal = thaw(actx, discr.normal(q_tpair.dd))
 
-    flux_out = flux_dis @ normal
+    flux_out = np.dot(q_tpair.avg,normal)
 
     return discr.project(q_tpair.dd, "all_faces", flux_out)
 
-def artificial_viscosity(discr, r):
+def artificial_viscosity(discr, t, eos, boundaries, r, alpha):
     r"""Compute artifical viscosity for the euler equations
 
     """
-
     #compute dissapation flux
-    vol_flux_r = dissapative_flux(discr, r)
-    dflux_r = discr.weak_div(vol_flux_r)
 
+    #Cannot call weak_grad on obj of nd arrays, use obj_array_vectorize as work around
+    dflux_r = obj_array_vectorize(discr.weak_grad,r)
+    
     #interior face flux
-    iff_r = _facial_flux(discr, q_tpair=interior_trace_pair(discr,r))
+
+    #Doesn't work: something related to obj on compute device
+    #Not 100% on reason
+    #qin = interior_trace_pair(discr,r)
+    #iff_r = _facial_flux(discr,q_tpair=qin)
+
+    #Work around?
+    def my_facialflux_r_interior(q):
+        qin = interior_trace_pair(discr,make_obj_array([q]))
+        return _facial_flux_r(discr,q_tpair=qin)
+
+    iff_r = obj_array_vectorize(my_facialflux_r_interior,r)
+    
 
     #partition boundaries flux
-    pbf_r = sum(
-        _facial_flux(discr, q_tpair=part_pair)
-        for part_pair in cross_rank_trace_pairs(discr, r)
-    )
+    #flux across partition boundaries
+    def my_facialflux_r_partition(q):
+        qin = cross_rank_trace_pairs(discr,q)
+        return  sum(_facial_flux_r(discr,q_tpair=part_pair) for part_pair in cross_rank_trace_pairs(discr,make_obj_array([q])) )
+    
+    pbf_r = obj_array_vectorize(my_facialflux_r_partition,r)
+    
+    #pbf_r = sum(
+    #    _facial_flux_r(discr, q_tpair=part_pair)
+    #    for part_pair in cross_rank_trace_pairs(discr, r)
+    #)
 
-    #domain boundary flux
-    dir_r = discr.project("vol", BTAG_ALL,r)
-    dbf_r = _facial_flux(
-            discr, 
-            q_tpair=TracePair(BTAG_ALL,interior=dir_r,exterior=dir_r)
-        )
+    #domain boundary flux basic boundary implementation
+    #def my_facialflux2(r):
+    #    dir_r = discr.project("vol", BTAG_ALL,make_obj_array([r]))
+    #    dbf_r = _facial_flux(
+    #         discr, 
+    #         q_tpair=TracePair(BTAG_ALL,interior=dir_r,exterior=dir_r)
+    #     )
+    #    return (dbf_r)
+    #dbf_r = obj_array_vectorize(my_facialflux2,r)
 
-    q = discr.inverse_mass( 1.0e-2 * (dflux_r - discr.face_mass(iff_r + pbf_r + dbf_r)))
+
+    #True boundary implementation
+    #Okay, not sure about this...
+    #What I am attempting:
+    #       1. Loop through all the boundaries
+    #       2. Define a function my_TP that performes the trace pair for the given boundary
+    #            given a solution variable
+    #       3. Get the external solution from the boundary routine
+    #       4. Get hte projected internal solution
+    #       5. Compute the boundary flux as a sum over boundaries, using the obj_array_vectorize to
+    #           pass each solution variable one at a time
+    # DO I really need to do this like this?
+    dbf_r = 0.0*iff_r
+    for btag in boundaries:
+            def my_facialflux_r_boundary(sol_ext,sol_int):
+                q_tpair = TracePair(btag,interior=make_obj_array([sol_int]),exterior=make_obj_array([sol_ext]))
+                return _facial_flux_r(discr,q_tpair=q_tpair)
+            r_ext=boundaries[btag].interior_sol(discr,eos=eos,btag=btag,t=t,q=r)
+            r_int=discr.project("vol",btag,r)
+            dbf_r = dbf_r + obj_array_vectorize_n_args(my_facialflux_r_boundary,r_ext,r_int)
+            
+        
+    #Compute q, half way done!
+    q = discr.inverse_mass( -alpha * (dflux_r - discr.face_mass(iff_r + pbf_r + dbf_r)))
 
     #flux of q
-    vol_flux_q = dissapative_flux(discr,  q)
-    dflux_q =  discr.weak_div(vol_flux_q)
+    
+    #Again we need to vectorize
+    #q is a object array of object arrays (dim,) of DOFArrays (?)
+    dflux_q =  obj_array_vectorize(discr.weak_div,q)
 
     #interior face flux of q
-    iff_q =  _facial_flux(discr, q_tpair=interior_trace_pair(discr,q))
+    def my_facialflux_q_interior(q):
+        qin = interior_trace_pair(discr,q)
+        iff_q =  _facial_flux_q(discr, q_tpair=qin)
+        return (iff_q)
+
+    iff_q = obj_array_vectorize(my_facialflux_q_interior,q)
+    
 
     #flux across partition boundaries
-    pbf_q = sum(
-        _facial_flux(discr, q_tpair=part_pair)
-        for part_pair in cross_rank_trace_pairs(discr, q)
-    )
-
-    #dombain boundary flux
-    dir_q = discr.project("vol",BTAG_ALL, q);
-    dbf_q = _facial_flux(
-            discr,
-            q_tpair=TracePair(BTAG_ALL,interior=dir_q,exterior=dir_q)
-        )
+    def my_facialflux_q_partition(q):
+        qin = cross_rank_trace_pairs(discr,q)
+        return  sum(_facial_flux_q(discr,q_tpair=part_pair) for part_pair in cross_rank_trace_pairs(discr,make_obj_array([q])) )
     
-    
-    return discr.inverse_mass(
-        dflux_q - discr.face_mass(iff_q + pbf_q + dbf_q)
-    )
+    pbf_q = obj_array_vectorize(my_facialflux_q_partition,q)
+    #pbf_q = sum(
+    #    _facial_flux_q(discr, q_tpair=part_pair)
+    #    for part_pair in cross_rank_trace_pairs(discr, q)
+    #)
 
+    def my_facialflux_q_boundary(q):
+        #dombain boundary flux
+        dir_q = discr.project("vol",BTAG_ALL, q);
+        dbf_q = _facial_flux_q(
+             discr,
+             q_tpair=TracePair(BTAG_ALL,interior=dir_q,exterior=dir_q)
+            )
+        return(dbf_q)
+
+    dbf_q = obj_array_vectorize(my_facialflux_q_boundary,q)
+
+    #Return the rhs contribution
+    return ( discr.inverse_mass( -dflux_q + discr.face_mass(iff_q + pbf_q + dbf_q) ) )
+   
