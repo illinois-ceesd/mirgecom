5604f33f706577c9e873cb02a09a9914caf26aed
Matthias Diener <mdiener@illinois.edu>
2020-11-16 10:48:19 -0600

    reset sod-mpi to master

--- examples/sod-mpi.py
+++ examples/sod-mpi.py
@@ -34,7 +34,6 @@ from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import EagerDGDiscretization
 from grudge.shortcuts import make_visualizer
 
-from mirgecom.profiling import PyOpenCLProfilingArrayContext
 
 from mirgecom.euler import inviscid_operator
 from mirgecom.simutil import (
@@ -52,36 +51,17 @@ from mirgecom.boundary import PrescribedBoundary
 from mirgecom.initializers import SodShock1D
 from mirgecom.eos import IdealSingleGas
 
-from logpyle import (LogManager, add_general_quantities,
-        add_simulation_quantities, add_run_info)
-
-from mirgecom.logging_quantities import PhysicalQuantity, KernelProfile
-
 
 logger = logging.getLogger(__name__)
 
 
 @mpi_entry_point
-def main(ctx_factory=cl.create_some_context, use_profiling=False, use_logmgr=False):
+def main(ctx_factory=cl.create_some_context):
     """Drive the example."""
-    if use_logmgr:
-        logmgr = LogManager("sod1d.dat", "wu")  # , comm=...
-        add_run_info(logmgr)
-        add_general_quantities(logmgr)
-        add_simulation_quantities(logmgr)
-    else:
-        logmgr = None
-
     cl_ctx = ctx_factory()
-    if use_profiling:
-        queue = cl.CommandQueue(cl_ctx,
-            properties=cl.command_queue_properties.PROFILING_ENABLE)
-        actx = PyOpenCLProfilingArrayContext(queue,
-            allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
-    else:
-        queue = cl.CommandQueue(cl_ctx)
-        actx = PyOpenCLArrayContext(queue,
-            allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
+    queue = cl.CommandQueue(cl_ctx)
+    actx = PyOpenCLArrayContext(queue,
+                allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
 
     dim = 1
     nel_1d = 24
@@ -120,22 +100,11 @@ def main(ctx_factory=cl.create_some_context, use_profiling=False, use_logmgr=Fal
     discr = EagerDGDiscretization(
         actx, local_mesh, order=order, mpi_communicator=comm
     )
-
     nodes = thaw(actx, discr.nodes())
     current_state = initializer(0, nodes)
 
-    if use_logmgr:
-        logmgr.add_quantity(PhysicalQuantity(discr, eos, "pressure", "P", "min"))
-        logmgr.add_quantity(PhysicalQuantity(discr, eos, "temperature", "K", "min"))
-        if rank == 0:
-            logmgr.add_watches(["step", "t_step", "min_pressure", "min_temperature"])
-        if use_profiling:
-            logmgr.add_quantity(KernelProfile(actx, "diff", "flops"))
-            if rank == 0:
-                logmgr.add_watches(["diff_flops"])
-
-    visualizer = make_visualizer(discr, order + 3
-                                 if discr.dim == 2 else order)
+    visualizer = make_visualizer(discr, discr.order + 3
+                                 if discr.dim == 2 else discr.order)
     initname = initializer.__class__.__name__
     eosname = eos.__class__.__name__
     init_message = make_init_message(dim=dim, order=order,
@@ -167,7 +136,7 @@ def main(ctx_factory=cl.create_some_context, use_profiling=False, use_logmgr=Fal
             advance_state(rhs=my_rhs, timestepper=timestepper,
                           checkpoint=my_checkpoint,
                           get_timestep=get_timestep, state=current_state,
-                          t=current_t, t_final=t_final, logmgr=logmgr)
+                          t=current_t, t_final=t_final)
     except ExactSolutionMismatch as ex:
         current_step = ex.step
         current_t = ex.t
@@ -183,22 +152,9 @@ def main(ctx_factory=cl.create_some_context, use_profiling=False, use_logmgr=Fal
     if current_t - t_final < 0:
         raise ValueError("Simulation exited abnormally")
 
-    if use_logmgr:
-        logmgr.close()
-    elif use_profiling:
-        print(actx.tabulate_profiling_data())
-
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(message)s", level=logging.INFO)
-    import argparse
-    parser = argparse.ArgumentParser(description="Sod (MPI version)")
-    parser.add_argument("--profile", action="store_true",
-        help="enable kernel profiling")
-    parser.add_argument("--logging", action="store_true",
-        help="enable time series logging")
-    args = parser.parse_args()
-
-    main(use_profiling=args.profile, use_logmgr=args.logging)
+    main()
 
 # vim: foldmethod=marker
