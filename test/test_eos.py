2145852d345f12378c88d5868e00be50bbb60fd4
Michael Campbell <mtcampbe@illinois.edu>
2021-01-15 11:10:55 -0600

    Update EOS tests to use new init interface

--- test/test_eos.py
+++ test/test_eos.py
@@ -80,7 +80,7 @@ def test_idealsingle_lump(ctx_factory):
     velocity[0] = 1
     lump = Lump(dim=dim, center=center, velocity=velocity)
     eos = IdealSingleGas()
-    lump_soln = lump(0, nodes)
+    lump_soln = lump(nodes)
 
     cv = split_conserved(dim, lump_soln)
     p = eos.pressure(cv)
@@ -130,7 +130,7 @@ def test_idealsingle_vortex(ctx_factory):
     eos = IdealSingleGas()
     # Init soln with Vortex
     vortex = Vortex2D()
-    vortex_soln = vortex(0, nodes)
+    vortex_soln = vortex(nodes)
     cv = split_conserved(dim, vortex_soln)
     gamma = eos.gamma()
     p = eos.pressure(cv)
