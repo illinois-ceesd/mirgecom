2d7c7ed4779835b9727b1ba33ce8f80f9c581a6b
Matthias Diener <mdiener@illinois.edu>
2021-01-21 08:53:06 -0600

    unquiet

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -15,9 +15,9 @@ MINIFORGE_INSTALL_SH=Miniforge3-$PLATFORM-x86_64.sh
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/$MINIFORGE_INSTALL_SH"
 rm -Rf "$MINIFORGE_INSTALL_DIR"
 bash "$MINIFORGE_INSTALL_SH" -b -p "$MINIFORGE_INSTALL_DIR"
-PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes --quiet
-PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes --quiet
-PATH="$MINIFORGE_INSTALL_DIR/bin:$PATH" conda env create --file .test-conda-env-py3.yml --name testing --quiet
+PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes
+PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes
+PATH="$MINIFORGE_INSTALL_DIR/bin:$PATH" conda env create --file .test-conda-env-py3.yml --name testing
 
 . "$MINIFORGE_INSTALL_DIR/bin/activate" testing
 
