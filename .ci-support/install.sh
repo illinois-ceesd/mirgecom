0fbd26bfa1c1b782666c12858e23241910d9d8b9
Matthias Diener <mdiener@illinois.edu>
2021-01-21 18:43:24 -0600

    pyopencl

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash -x
-
 if [ "$(uname)" = "Darwin" ]; then
 PLATFORM=MacOSX
 brew install open-mpi
@@ -15,15 +13,13 @@ MINIFORGE_INSTALL_SH=Miniforge3-$PLATFORM-x86_64.sh
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/$MINIFORGE_INSTALL_SH"
 rm -Rf "$MINIFORGE_INSTALL_DIR"
 bash "$MINIFORGE_INSTALL_SH" -b -p "$MINIFORGE_INSTALL_DIR"
-PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes
-PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes
-PATH="$MINIFORGE_INSTALL_DIR/bin:$PATH" conda env create --file .test-conda-env-py3.yml --name testing
+PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes --quiet
+PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes --quiet
+PATH="$MINIFORGE_INSTALL_DIR/bin:$PATH" conda env create --file .test-conda-env-py3.yml --name testing --quiet
 
-# . "$MINIFORGE_INSTALL_DIR/bin/activate" testing
+. "$MINIFORGE_INSTALL_DIR/bin/activate" testing
 
 MINIFORGE_INSTALL_DIR=.miniforge3
 . "$MINIFORGE_INSTALL_DIR/bin/activate" testing
-conda info --envs
-
-# pip install -r requirements.txt
+pip install -r requirements.txt
 python setup.py install
