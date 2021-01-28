b965c5bbeb4ac1f24027fc0ff0f0b33d4142b509
Andreas Kloeckner <inform@tiker.net>
2021-01-28 09:13:24 -0600

    Use **kwags in boundary_pair method

--- mirgecom/boundary.py
+++ mirgecom/boundary.py
@@ -58,15 +58,13 @@ class PrescribedBoundary:
         """
         self._userfunc = userfunc
 
-    def boundary_pair(
-            self, discr, q, btag, eos, t=0.0
-    ):
+    def boundary_pair(self, discr, q, btag, *args, **kwargs):
         """Get the interior and exterior solution on the boundary."""
         actx = q[0].array_context
 
         boundary_discr = discr.discr_from_dd(btag)
         nodes = thaw(actx, boundary_discr.nodes())
-        ext_soln = self._userfunc(nodes, t=t, eos=eos)
+        ext_soln = self._userfunc(nodes, **kwargs)
         int_soln = discr.project("vol", btag, q)
         return TracePair(btag, interior=int_soln, exterior=ext_soln)
 
@@ -77,9 +75,7 @@ class DummyBoundary:
     .. automethod:: boundary_pair
     """
 
-    def boundary_pair(
-            self, discr, q, btag, eos, t=0.0
-    ):
+    def boundary_pair(self, discr, q, btag, **kwargs):
         """Get the interior and exterior solution on the boundary."""
         dir_soln = discr.project("vol", btag, q)
         return TracePair(btag, interior=dir_soln, exterior=dir_soln)
@@ -103,9 +99,7 @@ class AdiabaticSlipBoundary:
     .. automethod:: boundary_pair
     """
 
-    def boundary_pair(
-            self, discr, q, btag, eos, t=0.0
-    ):
+    def boundary_pair(self, discr, q, btag, **kwargs):
         """Get the interior and exterior solution on the boundary.
 
         The exterior solution is set such that there will be vanishing
