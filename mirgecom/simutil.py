0cd792f8012a2af1f846056481cc498b831afcb5
Mike Campbell <mtcampbe@illinois.edu>
2021-02-08 12:08:45 -0600

    Allow visualization of custom fields. (#228)

--- mirgecom/simutil.py
+++ mirgecom/simutil.py
@@ -93,9 +93,8 @@ class ExactSolutionMismatch(Exception):
 
 def sim_checkpoint(discr, visualizer, eos, q, vizname, exact_soln=None,
                    step=0, t=0, dt=0, cfl=1.0, nstatus=-1, nviz=-1, exittol=1e-16,
-                   constant_cfl=False, comm=None, overwrite=False):
+                   constant_cfl=False, comm=None, viz_fields=None, overwrite=False):
     """Check simulation health, status, viz dumps, and restart."""
-    # TODO: Add restart
     do_viz = check_step(step=step, interval=nviz)
     do_status = check_step(step=step, interval=nstatus)
     if do_viz is False and do_status is False:
@@ -128,6 +127,8 @@ def sim_checkpoint(discr, visualizer, eos, q, vizname, exact_soln=None,
                 ("exact_soln", expected_state),
             ]
             io_fields.extend(exact_list)
+        if viz_fields is not None:
+            io_fields.extend(viz_fields)
 
         from mirgecom.io import make_rank_fname, make_par_fname
         rank_fn = make_rank_fname(basename=vizname, rank=rank, step=step, t=t)
