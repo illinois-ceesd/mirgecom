7460dfd79b2b4be064f1485e8e4db19e8bd29244
Michael Campbell <mtcampbe@illinois.edu>
2021-04-02 12:10:56 -0500

    Update test to reflect not taking gradients inside velgrad function

--- test/test_fluid.py
+++ test/test_fluid.py
@@ -49,13 +49,14 @@ logger = logging.getLogger(__name__)
 @pytest.mark.parametrize("dim", [1, 2, 3])
 @pytest.mark.parametrize(("mass_exp", "vel_fac"),
                          [(0, 0), (0, 1),
-                          (1, 1), (2, 1)])
+                          (1, 1), (2, 1),
+                          (0, 2), (2, 2)])
 def test_velocity_gradient_sanity(actx_factory, dim, mass_exp, vel_fac):
-    """Test that the grad(v) returns {0, I} for v={constant, r_xyz}."""
+    """Test that the grad(v) returns {0, a*I} for v={constant, a*r_xyz}."""
     from mirgecom.fluid import velocity_gradient
     actx = actx_factory()
 
-    nel_1d = 16
+    nel_1d = 25
 
     from meshmode.mesh.generation import generate_regular_rect_mesh
 
@@ -84,7 +85,7 @@ def test_velocity_gradient_sanity(actx_factory, dim, mass_exp, vel_fac):
 
     grad_v = velocity_gradient(discr, cv, grad_cv)
 
-    tol = 1e-12
+    tol = 1e-11
     exp_result = vel_fac * np.eye(dim) * ones
     grad_v_err = [discr.norm(grad_v[i] - exp_result[i], np.inf)
                   for i in range(dim)]
@@ -94,56 +95,44 @@ def test_velocity_gradient_sanity(actx_factory, dim, mass_exp, vel_fac):
 
 @pytest.mark.parametrize("dim", [1, 2, 3])
 def test_velocity_gradient_eoc(actx_factory, dim):
-    """Test that the velocity gradient converges at the proper rate."""
+    """Test the velocity gradient with a trig function."""
     from mirgecom.fluid import velocity_gradient
     actx = actx_factory()
 
     order = 3
 
-    from pytools.convergence import EOCRecorder
-    eoc = EOCRecorder()
+    npts_1d = 25
 
-    nel_1d_0 = 5
-    for hn1 in [1, 2, 3, 4]:
-
-        nel_1d = hn1 * (nel_1d_0 - 1) + 1
-        h = 1/(nel_1d-1)
-
-        from meshmode.mesh.generation import generate_regular_rect_mesh
-        mesh = generate_regular_rect_mesh(
-            a=(1.0,) * dim, b=(2.0,) * dim, n=(nel_1d,) * dim
-        )
-
-        discr = EagerDGDiscretization(actx, mesh, order=order)
-        nodes = thaw(actx, discr.nodes())
-        zeros = discr.zeros(actx)
-        energy = zeros + 2.5
-
-        mass = nodes[dim-1]*nodes[dim-1]
-        velocity = make_obj_array([actx.np.cos(nodes[i]) for i in range(dim)])
-        mom = mass*velocity
-
-        q = join_conserved(dim, mass=mass, energy=energy, momentum=mom)
-        cv = split_conserved(dim, q)
-
-        grad_q = obj_array_vectorize(discr.grad, q)
-        grad_cv = split_conserved(dim, grad_q)
-
-        grad_v = velocity_gradient(discr, cv, grad_cv)
-
-        def exact_grad_row(xdata, gdim, dim):
-            exact_grad_row = make_obj_array([zeros for _ in range(dim)])
-            exact_grad_row[gdim] = -actx.np.sin(xdata)
-            return exact_grad_row
-
-        comp_err = make_obj_array([
-            discr.norm(grad_v[i] - exact_grad_row(nodes[i], i, dim), np.inf)
-            for i in range(dim)])
-        err_max = comp_err.max()
-        eoc.add_data_point(h, err_max)
-
-    print(eoc)
-    assert (
-        eoc.order_estimate() >= order - 0.5
-        or eoc.max_error() < 1e-9
+    from meshmode.mesh.generation import generate_regular_rect_mesh
+    mesh = generate_regular_rect_mesh(
+        a=(1.0,) * dim, b=(2.0,) * dim, n=(npts_1d,) * dim
     )
+
+    discr = EagerDGDiscretization(actx, mesh, order=order)
+    nodes = thaw(actx, discr.nodes())
+    zeros = discr.zeros(actx)
+    energy = zeros + 2.5
+
+    mass = nodes[dim-1]*nodes[dim-1]
+    velocity = make_obj_array([actx.np.cos(nodes[i]) for i in range(dim)])
+    mom = mass*velocity
+
+    q = join_conserved(dim, mass=mass, energy=energy, momentum=mom)
+    cv = split_conserved(dim, q)
+
+    grad_q = obj_array_vectorize(discr.grad, q)
+    grad_cv = split_conserved(dim, grad_q)
+
+    grad_v = velocity_gradient(discr, cv, grad_cv)
+
+    def exact_grad_row(xdata, gdim, dim):
+        exact_grad_row = make_obj_array([zeros for _ in range(dim)])
+        exact_grad_row[gdim] = -actx.np.sin(xdata)
+        return exact_grad_row
+
+    tol = 1e-5
+    comp_err = make_obj_array([
+        discr.norm(grad_v[i] - exact_grad_row(nodes[i], i, dim), np.inf)
+        for i in range(dim)])
+    err_max = comp_err.max()
+    assert err_max < tol
