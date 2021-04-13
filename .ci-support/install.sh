185323c95cb180cbf329f713edb70898b58a365f
Matthias Diener <mdiener@illinois.edu>
2021-04-13 11:16:40 -0500

    prefer mpich

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -7,7 +7,7 @@ brew install octave
 else
 PLATFORM=Linux
 sudo apt-get update
-sudo apt-get -y install openmpi-bin libopenmpi-dev
+sudo apt-get -y install libmpich-dev mpich
 sudo apt-get -y install octave
 fi
 MINIFORGE_INSTALL_DIR=.miniforge3
