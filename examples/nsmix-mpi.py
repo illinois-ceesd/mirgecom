abb2d2848df085f4d3358d2285a8eea23f3c6014
Michael Campbell <mtcampbe@illinois.edu>
2021-04-15 13:02:14 -0500

    Fix outfile name collision with autoignition

--- examples/nsmix-mpi.py
+++ examples/nsmix-mpi.py
@@ -173,7 +173,7 @@ def main(ctx_factory=cl.create_some_context):
     # Create a Pyrometheus EOS with the Cantera soln. Pyrometheus uses Cantera and
     # generates a set of methods to calculate chemothermomechanical properties and
     # states for this particular mechanism.
-    casename = "autoignition"
+    casename = "nsmix"
     pyrometheus_mechanism = pyro.get_thermochem_class(cantera_soln)(actx.np)
     eos = PyrometheusMixture(pyrometheus_mechanism,
                              temperature_guess=init_temperature,
