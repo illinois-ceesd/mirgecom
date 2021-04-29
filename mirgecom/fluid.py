cafc01811de56dc1753cc3cedc5e0abed55c0245
Mike Campbell <mtcampbe@illinois.edu>
2021-04-29 07:25:04 -0500

    Give more precise info about velocity in doc example.
    Co-authored-by: Thomas H. Gibson <gibsonthomas1120@hotmail.com>

--- mirgecom/fluid.py
+++ mirgecom/fluid.py
@@ -151,7 +151,7 @@ class ConservedVars:
         quantities::
 
             den = rho  # rho is a DOFArray with fluid density
-            velo = v  # v is an obj array of ndim DOFArrays.
+            velo = v  # v is an obj array of ndim DOFArrays denoting the components of velocity.
             ener = e  # e is a DOFArray with fluid energy
             mom_dens = den*velo
             ener_dens = den*ener
