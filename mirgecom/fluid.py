3d73397a631a73484e16b43bdc26be2fa24489c9
Mike Campbell <mtcampbe@illinois.edu>
2021-06-05 00:21:22 -0500

    Add array container decorators for arraycontexted CV (#354)
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- mirgecom/fluid.py
+++ mirgecom/fluid.py
@@ -42,8 +42,17 @@ import numpy as np  # noqa
 from pytools.obj_array import make_obj_array
 from meshmode.dof_array import DOFArray  # noqa
 from dataclasses import dataclass
+from arraycontext import (
+    dataclass_array_container,
+    with_container_arithmetic,
+)
 
 
+@with_container_arithmetic(bcast_obj_array=False,
+                           bcast_container_types=(DOFArray, np.ndarray),
+                           matmul=True,
+                           rel_comparison=True)
+@dataclass_array_container
 @dataclass(frozen=True)
 class ConservedVars:
     r"""Store and resolve quantities according to the fluid conservation equations.
@@ -218,6 +227,11 @@ class ConservedVars:
     energy: DOFArray
     momentum: np.ndarray
     species_mass: np.ndarray = np.empty((0,), dtype=object)  # empty = immutable
+
+    @property
+    def array_context(self):
+        """Return an array context for the :class:`ConservedVars` object."""
+        return self.mass.array_context
 
     @property
     def dim(self):
