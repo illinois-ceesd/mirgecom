5f5798d4aa712a06d1e5ebd63e50af8824ac5317
Michael T. Campbell <mtcampbe@illinois.edu>
2021-06-21 19:17:49 -0700

    Override __reduce__ so CV will pickle correctly without cached items.

--- mirgecom/fluid.py
+++ mirgecom/fluid.py
@@ -42,7 +42,7 @@ THE SOFTWARE.
 import numpy as np  # noqa
 from pytools.obj_array import make_obj_array
 from meshmode.dof_array import DOFArray  # noqa
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from arraycontext import (
     dataclass_array_container,
     with_container_arithmetic,
@@ -239,6 +239,11 @@ class ConservedVars:
         """Return the number of physical dimensions."""
         return len(self.momentum)
 
+    def __reduce__(self):
+        """Return a tuple reproduction of self for pickling."""
+        return(ConservedVars, tuple(getattr(self, f.name)
+                                    for f in fields(ConservedVars)))
+
     def join(self):
         """Call :func:`join_conserved` on *self*."""
         return join_conserved(
@@ -252,6 +257,7 @@ class ConservedVars:
         """Return a copy of *self* with the attributes in *kwargs* replaced."""
         from dataclasses import replace
         return replace(self, **kwargs)
+
 
 
 def _aux_shape(ary, leading_shape):
