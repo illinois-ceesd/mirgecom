f23f2ff0b1dddd8bbf1ecf190c24cf9b0ae35d54
Michael Campbell <mtcampbe@illinois.edu>
2021-07-11 13:56:50 -0500

    Test using outstanding updates to production driver

--- .ci-support/production-setup.sh
+++ .ci-support/production-setup.sh
@@ -2,7 +2,7 @@ set -x
 PRODUCTION_CHANGE_OWNER="illinois-ceesd"
 PRODUCTION_CHANGE_BRANCH=""
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [ ${PRODUCTION_CHANGE_BRANCH} != "" ]; then
+if [ -n "${PRODUCTION_CHANGE_BRANCH}" ]; then
     git remote add production_change https://github.com/${PRODUCTION_CHANGE_OWNER}/mirgecom
     git fetch production_change
     git checkout production_change/${PRODUCTION_CHANGE_BRANCH}
