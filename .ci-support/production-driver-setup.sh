9dea1588827e8bdd5b7b3d893c80837569931555
Michael Campbell <mtcampbe@illinois.edu>
2021-07-12 13:46:46 -0500

    Update Y1 CI testing so that it also working in main.

--- .ci-support/production-driver-setup.sh
+++ .ci-support/production-driver-setup.sh
@@ -6,6 +6,8 @@ PRODUCTION_DRIVER_NAME="drivers_y1-nozzle"
 PRODUCTION_DRIVER_CHANGE_OWNER="illinois-ceesd"
 PRODUCTION_DRIVER_CHANGE_BRANCH="update-y1-callbacks"
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git config user.email "stupid@dumb.com"
+git config user.name "CI Runner"
 if [ -n "${PRODUCTION_DRIVER_CHANGE_BRANCH}" ]; then
     git remote add driver_change https://github.com/${PRODUCTION_DRIVER_CHANGE_OWNER}/${PRODUCTION_DRIVER_NAME}
     git fetch driver_change
