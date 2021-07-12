18eb7146fca4ad4453926471430afbcc4c0eda37
Jacob Levine <jacoblevine18@gmail.com>
2021-07-12 15:05:39 -0500

    Fix test

--- test/test_viscous.py
+++ test/test_viscous.py
@@ -274,7 +274,7 @@ def test_local_max_species_diffusivity(actx_factory, dim):
     eos = IdealSingleGas(transport_model=tv_model)
 
     from mirgecom.viscous import get_local_max_species_diffusivity
-    expected = get_local_max_species_diffusivity(actx, tv_model, eos, cv)
+    expected = get_local_max_species_diffusivity(tv_model, eos, cv)
 
     assert((expected == .3*ones[0]).all())
 
