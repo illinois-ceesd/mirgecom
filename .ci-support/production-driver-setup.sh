cfe702a8ae49e462a89041310f598c73edd4c0dd
Mike Campbell <mtcampbe@illinois.edu>
2021-07-12 13:24:01 -0500

    Add Y1 production test to CI (#423)
    Co-authored-by: Matthias Diener <mdiener@illinois.edu>

---
+++ .ci-support/production-driver-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+PRODUCTION_DRIVER_NAME="drivers_y1-nozzle"
+PRODUCTION_DRIVER_CHANGE_OWNER="illinois-ceesd"
+PRODUCTION_DRIVER_CHANGE_BRANCH="update-y1-callbacks"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ -n "${PRODUCTION_DRIVER_CHANGE_BRANCH}" ]; then
+    git remote add driver_change https://github.com/${PRODUCTION_DRIVER_CHANGE_OWNER}/${PRODUCTION_DRIVER_NAME}
+    git fetch driver_change
+    git checkout driver_change/${PRODUCTION_DRIVER_CHANGE_BRANCH}
+    git checkout ${CURRENT_BRANCH}
+    git merge driver_change/${PRODUCTION_DRIVER_CHANGE_BRANCH} --no-edit
+else
+    echo "No updates to production driver branch (${CURRENT_BRANCH})"
+fi
