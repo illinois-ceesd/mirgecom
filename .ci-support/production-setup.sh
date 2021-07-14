257f18d8bdc784cc348cf8cb8e9d923a723afd76
Michael Campbell <mtcampbe@illinois.edu>
2021-07-14 14:24:39 -0500

    Update production testing setup to be slightly more robust against wicked merges that try to change them.

--- .ci-support/production-setup.sh
+++ .ci-support/production-setup.sh
@@ -7,6 +7,9 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git config user.email "stupid@dumb.com"
 git config user.name "CI Runner"
 if [ -n "${PRODUCTION_CHANGE_BRANCH}" ]; then
+    if [ -z "${PRODUCTION_CHANGE_OWNER}"]; then
+        PRODUCTION_CHANGE_OWNER="illinois-ceesd"
+    fi
     git remote add production_change https://github.com/${PRODUCTION_CHANGE_OWNER}/mirgecom
     git fetch production_change
     git checkout production_change/${PRODUCTION_CHANGE_BRANCH}
@@ -15,8 +18,11 @@ if [ -n "${PRODUCTION_CHANGE_BRANCH}" ]; then
 else
     echo "No updates to production branch (${CURRENT_BRANCH})"
 fi
-CURRENT_FORK_OWNER="majosm"
+CURRENT_FORK_OWNER=""
 if [ -n "${GITHUB_HEAD_REF}" ]; then
+    if [ -z "${CURRENT_FORK_OWNER}"]; then
+        CURRENT_FORK_OWNER="illinois-ceesd"
+    fi
     git remote add changes https://github.com/${CURRENT_FORK_OWNER}/mirgecom
     git fetch changes
     git checkout changes/${GITHUB_HEAD_REF}
