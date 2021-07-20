3442a50453e1333202145412fa0629cf66377412
Michael Campbell <mtcampbe@illinois.edu>
2021-07-19 21:43:28 -0500

    Improve the runner script which was reporting spurious successful tests.

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -16,35 +16,39 @@ do
     then
         echo "*** Running parallel lazy example (1 rank): $example"
         mpiexec -n 1 python -m mpi4py ${example} --lazy
-        rm -rf *vtu *sqlite *pkl *-journal restart_data
     elif [[ "$example" == *"-mpi.py" ]]; then
         echo "*** Running parallel example (2 ranks): $example"
         mpiexec -n 2 python -m mpi4py ${example}
-        rm -rf *vtu *sqlite *pkl *-journal restart_data
     elif [[ "$example" == *"-lazy.py" ]]; then
         echo "*** Running serial lazy example: $example"
         python ${example} --lazy
-        rm -rf *vtu *sqlite *pkl *-journal restart_data
     else
         echo "*** Running serial example: $example"
         python ${example}
-        rm -rf *vtu *sqlite *pkl *-journal restart_data
     fi
     if [[ $? -eq 0 ]]
     then
+        ((numsuccess=numsuccess+1))
         echo "*** Example $example succeeded."
+        succeeded_examples="$succeeded_examples $example"
     else
         ((exitcode=exitcode+1))
         echo "*** Example $example failed."
         failed_examples="$failed_examples $example"
     fi
+    rm -rf *vtu *sqlite *pkl *-journal restart_data
 done
+((numtests=numsuccess+exitcode))
 echo "*** Done running examples!"
 if [[ $exitcode -eq 0 ]]
 then
     echo "*** No errors."
+    echo "*** $numtests successful tests."
+    echo "*** Test names: $succeeded_examples."
 else
-    echo "*** Errors detected ($exitcode):($failed_examples )"
+    echo "*** Errors detected," 
+    echo "*** Failed tests: ($exitcode/$numtests): $failed_examples"
+    echo "*** Successful tests: ($numsuccess/$numtests): $succeeded_examples"
     exit $exitcode
 fi
 #rm -f examples/*.vtu
