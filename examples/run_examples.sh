0c7a054609277bdbb3760873c8f5d780650a96da
Michael Campbell <mtcampbe@illinois.edu>
2021-07-20 09:17:47 -0500

    Match run_examples.sh with wave-lazy branch.

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -7,7 +7,7 @@ rm -f *.vtu *.pvtu
 
 origin=$(pwd)
 examples_dir=${1-$origin}
-declare -i exitcode=0
+declare -i numfail=0
 declare -i numsuccess=0
 echo "*** Running examples in $examples_dir ..."
 failed_examples=""
@@ -34,23 +34,21 @@ do
         echo "*** Example $example succeeded."
         succeeded_examples="$succeeded_examples $example"
     else
-        ((exitcode=exitcode+1))
+        ((numfail=numfail+1))
         echo "*** Example $example failed."
         failed_examples="$failed_examples $example"
     fi
     rm -rf *vtu *sqlite *pkl *-journal restart_data
 done
-((numtests=numsuccess+exitcode))
+((numtests=numsuccess+numfail))
 echo "*** Done running examples!"
-if [[ $exitcode -eq 0 ]]
+if [[ $numfail -eq 0 ]]
 then
     echo "*** No errors."
-    echo "*** $numtests successful tests."
-    echo "*** Test names: $succeeded_examples."
 else
-    echo "*** Errors detected," 
-    echo "*** Failed tests: ($exitcode/$numtests): $failed_examples"
-    echo "*** Successful tests: ($numsuccess/$numtests): $succeeded_examples"
-    exit $exitcode
+    echo "*** Errors detected." 
+    echo "*** Failed tests: ($numfail/$numtests): $failed_examples"
 fi
+echo "*** Successful tests: ($numsuccess/$numtests): $succeeded_examples"
+exit $numfail
 #rm -f examples/*.vtu
