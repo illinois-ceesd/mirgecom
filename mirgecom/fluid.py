Correct and test the product rule gradients

mirgecom/fluid.py
@@ -391,10 +391,14 @@ def velocity_gradient(discr, cv, grad_cv):
         \partial_{x}\mathbf{v}_{y}&\partial_{y}\mathbf{v}_{y} \end{array} \right)$
 
     """
-    velocity = cv.momentum / cv.mass
-    return (1/cv.mass)*make_obj_array([grad_cv.momentum[i]
-                                       - velocity[i]*grad_cv.mass
+    vel = cv.velocity
+    obj_ary = (1/cv.mass)*make_obj_array([grad_cv.momentum[i]
+                                       - vel[i]*grad_cv.mass
                                        for i in range(cv.dim)])
+    grad_v = np.empty(shape=(cv.dim, cv.dim), dtype=object)
+    for idx, v in enumerate(obj_ary):
+        grad_v[idx] = v
+    return grad_v
 
 
 def species_mass_fraction_gradient(discr, cv, grad_cv):
@@ -427,9 +431,13 @@ def species_mass_fraction_gradient(discr, cv, grad_cv):
     """
     nspecies = len(cv.species_mass)
     y = cv.species_mass / cv.mass
-    return (1/cv.mass)*make_obj_array([grad_cv.species_mass[i]
+    obj_ary = (1/cv.mass)*make_obj_array([grad_cv.species_mass[i]
                                        - y[i]*grad_cv.mass
                                        for i in range(nspecies)])
+    grad_y = np.empty(shape=(nspecies, cv.dim), dtype=object)
+    for idx, v in enumerate(obj_ary):
+        grad_y[idx] = v
+    return grad_y
 
 
 def compute_wavespeed(eos, cv: ConservedVars):