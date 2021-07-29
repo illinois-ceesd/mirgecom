Correct and test the product rule gradients (#457)
Co-authored-by: Thomas H. Gibson <gibsonthomas1120@hotmail.com>
Co-authored-by: Matt Smith <mjsmith6@illinois.edu>

test/test_fluid.py
@@ -31,10 +31,7 @@ import pyopencl.clmath  # noqa
 import logging
 import pytest
 
-from pytools.obj_array import (
-    make_obj_array,
-    obj_array_vectorize
-)
+from pytools.obj_array import make_obj_array
 
 from meshmode.dof_array import thaw
 from mirgecom.fluid import make_conserved
@@ -77,11 +74,13 @@ def test_velocity_gradient_sanity(actx_factory, dim, mass_exp, vel_fac):
     mom = mass * velocity
 
     cv = make_conserved(dim, mass=mass, energy=energy, momentum=mom)
-    grad_cv = make_conserved(dim, q=obj_array_vectorize(discr.grad, cv.join()))
+    from grudge.op import local_grad
+    grad_cv = make_conserved(dim,
+                             q=local_grad(discr, cv.join()))
 
     grad_v = velocity_gradient(discr, cv, grad_cv)
 
-    tol = 1e-12
+    tol = 1e-11
     exp_result = vel_fac * np.eye(dim) * ones
     grad_v_err = [discr.norm(grad_v[i] - exp_result[i], np.inf)
                   for i in range(dim)]
@@ -121,7 +120,9 @@ def test_velocity_gradient_eoc(actx_factory, dim):
         mom = mass*velocity
 
         cv = make_conserved(dim, mass=mass, energy=energy, momentum=mom)
-        grad_cv = make_conserved(dim, q=obj_array_vectorize(discr.grad, cv.join()))
+        from grudge.op import local_grad
+        grad_cv = make_conserved(dim,
+                                 q=local_grad(discr, cv.join()))
         grad_v = velocity_gradient(discr, cv, grad_cv)
 
         def exact_grad_row(xdata, gdim, dim):
@@ -140,3 +141,104 @@ def test_velocity_gradient_eoc(actx_factory, dim):
         eoc.order_estimate() >= order - 0.5
         or eoc.max_error() < 1e-9
     )
+
+
+def test_velocity_gradient_structure(actx_factory):
+    """Test gradv data structure, verifying usability with other helper routines."""
+    from mirgecom.fluid import velocity_gradient
+    actx = actx_factory()
+    dim = 3
+    nel_1d = 4
+
+    from meshmode.mesh.generation import generate_regular_rect_mesh
+
+    mesh = generate_regular_rect_mesh(
+        a=(1.0,) * dim, b=(2.0,) * dim, nelements_per_axis=(nel_1d,) * dim
+    )
+
+    order = 1
+
+    discr = EagerDGDiscretization(actx, mesh, order=order)
+    nodes = thaw(actx, discr.nodes())
+    zeros = discr.zeros(actx)
+    ones = zeros + 1.0
+
+    mass = 2*ones
+
+    energy = zeros + 2.5
+    velocity_x = nodes[0] + 2*nodes[1] + 3*nodes[2]
+    velocity_y = 4*nodes[0] + 5*nodes[1] + 6*nodes[2]
+    velocity_z = 7*nodes[0] + 8*nodes[1] + 9*nodes[2]
+    velocity = make_obj_array([velocity_x, velocity_y, velocity_z])
+
+    mom = mass * velocity
+
+    cv = make_conserved(dim, mass=mass, energy=energy, momentum=mom)
+    from grudge.op import local_grad
+    grad_cv = make_conserved(dim,
+                             q=local_grad(discr, cv.join()))
+    grad_v = velocity_gradient(discr, cv, grad_cv)
+
+    tol = 1e-11
+    exp_result = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    exp_trans = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    exp_trace = 15
+    assert grad_v.shape == (dim, dim)
+    from meshmode.dof_array import DOFArray
+    assert type(grad_v[0, 0]) == DOFArray
+    assert discr.norm(grad_v - exp_result, np.inf) < tol
+    assert discr.norm(grad_v.T - exp_trans, np.inf) < tol
+    assert discr.norm(np.trace(grad_v) - exp_trace, np.inf) < tol
+
+
+@pytest.mark.parametrize("dim", [1, 2, 3])
+def test_species_mass_gradient(actx_factory, dim):
+    """Test gradY structure and values against exact."""
+    actx = actx_factory()
+    nel_1d = 4
+
+    from meshmode.mesh.generation import generate_regular_rect_mesh
+    mesh = generate_regular_rect_mesh(
+        a=(1.0,) * dim, b=(2.0,) * dim, nelements_per_axis=(nel_1d,) * dim
+    )
+
+    order = 1
+
+    discr = EagerDGDiscretization(actx, mesh, order=order)
+    nodes = thaw(actx, discr.nodes())
+    zeros = discr.zeros(actx)
+    ones = zeros + 1
+
+    nspecies = 2*dim
+    mass = 2*ones  # make mass != 1
+    energy = zeros + 2.5
+    velocity = make_obj_array([ones for _ in range(dim)])
+    mom = mass * velocity
+    # assemble y so that each one has simple, but unique grad components
+    y = make_obj_array([ones for _ in range(nspecies)])
+    for idim in range(dim):
+        ispec = 2*idim
+        y[ispec] = ispec*(idim*dim+1)*sum([(iidim+1)*nodes[iidim]
+                                           for iidim in range(dim)])
+        y[ispec+1] = -y[ispec]
+    species_mass = mass*y
+
+    cv = make_conserved(dim, mass=mass, energy=energy, momentum=mom,
+                        species_mass=species_mass)
+    from grudge.op import local_grad
+    grad_cv = make_conserved(dim,
+                             q=local_grad(discr, cv.join()))
+    from mirgecom.fluid import species_mass_fraction_gradient
+    grad_y = species_mass_fraction_gradient(discr, cv, grad_cv)
+
+    assert grad_y.shape == (nspecies, dim)
+    from meshmode.dof_array import DOFArray
+    assert type(grad_y[0, 0]) == DOFArray
+
+    tol = 1e-11
+    for idim in range(dim):
+        ispec = 2*idim
+        exact_grad = np.array([(ispec*(idim*dim+1))*(iidim+1)
+                                for iidim in range(dim)])
+        assert discr.norm(grad_y[ispec] - exact_grad, np.inf) < tol
+        assert discr.norm(grad_y[ispec+1] + exact_grad, np.inf) < tol