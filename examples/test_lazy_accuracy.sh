642ff791fc3c4191af6502c2e929678f31edcd9d
Mike Campbell <mtcampbe@illinois.edu>
2021-09-17 15:28:24 -0500

    Egdun just a bit.

--- examples/test_lazy_accuracy.sh
+++ examples/test_lazy_accuracy.sh
@@ -7,5 +7,5 @@ set -o pipefail
 test_list="vortex-mpi.py pulse-mpi.py"
 for file in ${test_list}
 do
-    . ./compare_lazy_solution.sh ${file} 1e-13
+    . ./compare_lazy_solution.sh ${file} 5e-14
 done
