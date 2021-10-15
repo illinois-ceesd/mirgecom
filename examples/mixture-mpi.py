56ea46e61dbebfdeabbc811b3fdde67ec5f83862
Matt Smith <mjsmith6@illinois.edu>
2021-10-15 09:52:30 -0500

    refactor allsync (#521)
    * rename to global_reduce
    * remove MPI ops from interface so it can be called without importing mpi4py.MPI

--- examples/mixture-mpi.py
+++ examples/mixture-mpi.py
@@ -91,6 +91,9 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
     rank = comm.Get_rank()
     nparts = comm.Get_size()
 
+    from mirgecom.simutil import global_reduce as _global_reduce
+    global_reduce = partial(_global_reduce, comm=comm)
+
     logmgr = initialize_logmgr(use_logmgr,
         filename=f"{casename}.sqlite", mode="wu", mpi_comm=comm)
 
@@ -295,9 +298,8 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
                 exact = initializer(x_vec=nodes, eos=eos, time=t)
                 from mirgecom.simutil import compare_fluid_solutions
                 component_errors = compare_fluid_solutions(discr, state, exact)
-                from mirgecom.simutil import allsync
-                health_errors = allsync(my_health_check(dv, component_errors), comm,
-                                        op=MPI.LOR)
+                health_errors = global_reduce(
+                    my_health_check(dv, component_errors), op="lor")
                 if health_errors:
                     if rank == 0:
                         logger.info("Fluid solution failed health check.")
