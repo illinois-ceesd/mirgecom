e93a63f5c219b18838f45b0ac4525c98b72767b6
Matthias Diener <mdiener@illinois.edu>
2021-10-27 14:26:56 -0500

    try openmpi

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -1,14 +1,17 @@
-if [ "$(uname)" = "Darwin" ]; then
-PLATFORM=MacOSX
-brew update
-brew install open-mpi
-brew install octave
+#!/bin/bash
+
+if [[ "$(uname)" = "Darwin" ]]; then
+    PLATFORM=MacOSX
+    brew update
+    brew install open-mpi
+    brew install octave
 else
-PLATFORM=Linux
-sudo apt-get update
-sudo apt-get -y install libmpich-dev mpich
-sudo apt-get -y install octave
+    PLATFORM=Linux
+    sudo apt-get update
+    sudo apt-get -y install openmpi-bin libopenmpi-dev
+    sudo apt-get -y install octave
 fi
+
 MINIFORGE_INSTALL_DIR=.miniforge3
 MINIFORGE_INSTALL_SH=Miniforge3-$PLATFORM-x86_64.sh
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/$MINIFORGE_INSTALL_SH"
@@ -18,10 +21,11 @@ PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes --quiet
 PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes --quiet
 PATH="$MINIFORGE_INSTALL_DIR/bin:$PATH" conda env create --file conda-env.yml --name testing --quiet
 
-. "$MINIFORGE_INSTALL_DIR/bin/activate" testing
+source "$MINIFORGE_INSTALL_DIR/bin/activate" testing
 conda list
 
 MINIFORGE_INSTALL_DIR=.miniforge3
-. "$MINIFORGE_INSTALL_DIR/bin/activate" testing
+source "$MINIFORGE_INSTALL_DIR/bin/activate" testing
+
 pip install -r requirements.txt
 python setup.py install
