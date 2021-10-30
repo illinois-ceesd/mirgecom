57adc5212eac1a2ae91e23c9e7193df3886ed79f
Michael Campbell <mtcampbe@illinois.edu>
2021-10-29 20:58:11 -0500

    Breakout functionality for hand-running.

---
+++ .ci-support/merge-install-production-branch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -x
+#
+# This script is designed to patch the CEESD production capability against
+# a proposed change to illinois-ceesd/mirgecom@main. The script reads the
+# environment config file `.ci-support/production-testing-env.sh`, that
+# should set up the expected control variables, which are as follows:
+#
+# The proposed changes to test may be in a fork, or a local branch. For
+# forks, the environment config files should set:
+#
+# DEVELOPMENT_FORK = The development fork (default=illinois-ceesd)
+#
+# The production capability to test against may be specified outright, or
+# patched by the incoming development. The following vars control the
+# production environment:
+#
+# PRODUCTION_BRANCH = The production branch (default=y1-production)
+# PRODUCTION_FORK = The production fork (default=illinois-ceesd)
+#
+# If the environment file does not exist, the current development is
+# tested against `mirgecom@y1-production`.
+#
+MIRGE_HOME=${1:-"."}
+PRODUCTION_BRANCH=${PRODUCTION_BRANCH:-"y1-production"}
+PRODUCTION_FORK=${PRODUCTION_FORK:-"illinois-ceesd"}
+
+echo "MIRGE_HOME=${MIRGE_HOME}"
+echo "PRODUCTION_FORK=$PRODUCTION_FORK"
+echo "PRODUCTION_BRANCH=$PRODUCTION_BRANCH"
+
+cd ${MIRGE_HOME}
+git status
+
+# This junk is needed to be able to execute git commands properly
+git config user.email "ci-runner@ci.machine.com"
+git config user.name "CI Runner"
+
+# Making a dedicated production remote adds production forks
+git remote add production https://github.com/${PRODUCTION_FORK}/mirgecom
+git fetch production
+
+# Merge the production branch for testing the production drivers
+git merge production/${PRODUCTION_BRANCH} --no-edit
+# Pick up any requirements.txt
+pip install -r requirements.txt
+cd -
