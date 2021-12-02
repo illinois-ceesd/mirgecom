085b33364af0b05e7f848663ba81e74d05a3ac43
CI Runner <ci-runner@ci.machine.com>
2021-12-01 21:01:06 -0600

    Add gas model module

---
+++ mirgecom/gas_model.py
@@ -0,0 +1,279 @@
+""":mod:`mirgecom.gas_model` provides utilities to deal with gases.
+
+Fluid State Handling
+^^^^^^^^^^^^^^^^^^^^
+
+.. autoclass:: FluidState
+.. autofunction: make_fluid_state
+.. autofunction: project_fluid_state
+.. autofunction: make_fluid_state_trace_pairs
+
+"""
+
+__copyright__ = """
+Copyright (C) 2021 University of Illinois Board of Trustees
+"""
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import numpy as np  # noqa
+from meshmode.dof_array import DOFArray  # noqa
+from dataclasses import dataclass
+from arraycontext import dataclass_array_container
+from mirgecom.fluid import ConservedVars
+from mirgecom.eos import (
+    GasEOS,
+    EOSDependentVars,
+    MixtureDependentVars,
+    MixtureEOSError
+)
+from mirgecom.transport import (
+    TransportModel,
+    TransportModelError,
+    TransportDependentVars
+)
+
+
+@dataclass(frozen=True)
+class GasModel:
+    r"""Physical gas model for calculating fluid state-dependent quantities.
+
+    .. attribute:: eos
+    .. attribute:: transport_model
+    """
+
+    eos: GasEOS
+    transport: TransportModel = None
+
+
+@dataclass_array_container
+@dataclass(frozen=True)
+class FluidState:
+    r"""Gas model-consistent fluid state.
+
+    .. attribute:: cv
+
+        :class:`~mirgecom.fluid.ConservedVars` for the fluid conserved state
+
+    .. attribute:: dv
+
+        :class:`~mirgecom.eos.EOSDependentVars` for the fluid state-dependent
+        quantities corresponding to the chosen equation of state.
+
+    .. attribute:: tv
+        :class:`~mirgecom.transport.TransportDependentVars` for the fluid
+        state-dependent transport properties.
+
+    .. autoattribute:: array_context
+    .. autoattribute:: dim
+    .. autoattribute:: nspecies
+    .. autoattribute:: pressure
+    .. autoattribute:: temperature
+    .. autoattribute:: velocity
+    .. autoattribute:: speed
+    .. autoattribute:: wavespeed
+    .. autoattribute:: speed_of_sound
+    .. autoattribute:: mass_density
+    .. autoattribute:: momentum_density
+    .. autoattribute:: energy_density
+    .. autoattribute:: species_mass_density
+    .. autoattribute:: species_mass_fractions
+    """
+
+    cv: ConservedVars
+    dv: EOSDependentVars
+    tv: TransportDependentVars = None
+
+    @property
+    def array_context(self):
+        """Return the relevant array context for this object."""
+        return self.cv.array_context
+
+    @property
+    def dim(self):
+        """Return the number of physical dimensions."""
+        return self.cv.dim
+
+    @property
+    def nspecies(self):
+        """Return the number of physical dimensions."""
+        return self.cv.nspecies
+
+    @property
+    def pressure(self):
+        """Return the gas pressure."""
+        return self.dv.pressure
+
+    @property
+    def temperature(self):
+        """Return the gas temperature."""
+        return self.dv.temperature
+
+    @property
+    def mass_density(self):
+        """Return the gas density."""
+        return self.cv.mass
+
+    @property
+    def momentum_density(self):
+        """Return the gas momentum density."""
+        return self.cv.momentum
+
+    @property
+    def energy_density(self):
+        """Return the gas total energy density."""
+        return self.cv.energy
+
+    @property
+    def species_mass_density(self):
+        """Return the gas species densities."""
+        return self.cv.species_mass
+
+    @property
+    def velocity(self):
+        """Return the gas velocity."""
+        return self.cv.velocity
+
+    @property
+    def speed(self):
+        """Return the gas speed."""
+        return self.cv.speed
+
+    @property
+    def species_mass_fractions(self):
+        """Return the species mass fractions y = species_mass / mass."""
+        return self.cv.species_mass_fractions
+
+    @property
+    def speed_of_sound(self):
+        """Return the speed of sound in the gas."""
+        return self.dv.speed_of_sound
+
+    @property
+    def wavespeed(self):
+        """Return the characteristic wavespeed."""
+        return self.cv.speed + self.dv.speed_of_sound
+
+    @property
+    def has_transport(self):
+        """Indicate if this is a viscous state."""
+        return self.tv is not None
+
+    @property
+    def is_mixture(self):
+        """Indicate if this is a state resulting from a mixture gas model."""
+        return isinstance(self.dv, MixtureDependentVars)
+
+    def _get_transport_property(self, name):
+        """Grab a transport property if transport model is present."""
+        if not self.has_transport:
+            raise TransportModelError("Viscous transport model not provided.")
+        return getattr(self.tv, name)
+
+    def _get_mixture_property(self, name):
+        """Grab a mixture property if EOS is a :class:`~mirgecom.eos.MixtureEOS`."""
+        if not self.is_mixture:
+            raise MixtureEOSError("Mixture EOS required for mixture properties.")
+        return getattr(self.dv, name)
+
+    @property
+    def viscosity(self):
+        """Return the fluid viscosity."""
+        return self._get_transport_property("viscosity")
+
+    @property
+    def bulk_viscosity(self):
+        """Return the fluid bulk viscosity."""
+        return self._get_transport_property("bulk_viscosity")
+
+    @property
+    def thermal_conductivity(self):
+        """Return the fluid thermal conductivity."""
+        return self._get_transport_property("thermal_conductivity")
+
+    @property
+    def species_diffusivity(self):
+        """Return the fluid species diffusivities."""
+        return self._get_transport_property("species_diffusivity")
+
+    @property
+    def species_enthalpies(self):
+        """Return the fluid species diffusivities."""
+        return self._get_mixture_property("species_enthalpies")
+
+
+def make_fluid_state(cv, gas_model, temperature_seed=None):
+    """Create a fluid state from the conserved vars and equation of state."""
+    dv = gas_model.eos.dependent_vars(cv, temperature_seed=temperature_seed)
+    tv = None
+    if gas_model.transport is not None:
+        tv = gas_model.transport.dependent_vars(eos=gas_model.eos, cv=cv)
+    return FluidState(cv=cv, dv=dv, tv=tv)
+
+
+def project_fluid_state(discr, btag, state, gas_model):
+    """Project a fluid state onto a boundary consistent with the gas model.
+
+    If required by the gas model, (e.g. gas is a mixture), this routine will
+    ensure that the returned state is thermally consistent.
+    """
+    cv_sd = discr.project("vol", btag, state.cv)
+    temperature_seed = None
+    if state.is_mixture > 0:
+        temperature_seed = discr.project("vol", btag, state.dv.temperature)
+    return make_fluid_state(cv=cv_sd, gas_model=gas_model,
+                            temperature_seed=temperature_seed)
+
+
+def _getattr_ish(obj, name):
+    if obj is None:
+        return None
+    else:
+        return getattr(obj, name)
+
+
+def make_fluid_state_trace_pairs(cv_pairs, gas_model, temperature_seed_pairs=None):
+    """Create a fluid state from the conserved vars and equation of state."""
+    from grudge.trace_pair import TracePair
+    if temperature_seed_pairs is None:
+        temperature_seed_pairs = [None] * len(cv_pairs)
+    return [TracePair(
+        cv_pair.dd,
+        interior=make_fluid_state(cv_pair.int, gas_model,
+                                  temperature_seed=_getattr_ish(tseed_pair, "int")),
+        exterior=make_fluid_state(cv_pair.ext, gas_model,
+                                  temperature_seed=_getattr_ish(tseed_pair, "ext")))
+        for cv_pair, tseed_pair in zip(cv_pairs, temperature_seed_pairs)]
+
+
+def make_fluid_state_interior_trace_pair(discr, state, gas_model):
+    """Create a fluid state from the conserved vars and equation of state."""
+    from grudge.eager import interior_trace_pair
+    from grudge.trace_pair import TracePair
+    cv_tpair = interior_trace_pair(discr, state.cv)
+    tseed_pair = None
+    if state.nspecies > 0:
+        tseed_pair = interior_trace_pair(discr, state.dv.temperature)
+    return TracePair(
+        cv_tpair.dd,
+        interior=make_fluid_state(cv_tpair.int, gas_model,
+                                  _getattr_ish(tseed_pair, "int")),
+        exterior=make_fluid_state(cv_tpair.ext, gas_model,
+                                  _getattr_ish(tseed_pair, "ext")))
