1cca2e953cae4d7fdac011a19f06985afa851ab7
MTCam <mtcampbe@illinois.edu>
2021-12-07 11:43:49 -0600

    use generic lfr flux driver

--- mirgecom/inviscid.py
+++ mirgecom/inviscid.py
@@ -4,7 +4,8 @@ Inviscid Flux Calculation
 ^^^^^^^^^^^^^^^^^^^^^^^^^
 
 .. autofunction:: inviscid_flux
-.. autofunction:: inviscid_facial_divergence_flux
+.. autofunction:: inviscid_facial_flux
+.. autofunction:: inviscid_flux_rusanov
 
 Inviscid Time Step Computation
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -38,9 +39,7 @@ THE SOFTWARE.
 """
 
 import numpy as np
-from meshmode.dof_array import thaw
-from grudge.trace_pair import TracePair
-from mirgecom.flux import divergence_flux_lfr
+from mirgecom.flux import lfr_flux_driver
 from mirgecom.fluid import make_conserved
 
 
@@ -84,42 +83,10 @@ def inviscid_flux(state):
                           momentum=mom_flux, species_mass=species_mass_flux)
 
 
-def _lfr_flux(cv_pair, flux_pair, lam):
-    """Fluid state interface for Lax-Frierichs/Rusanov flux."""
-    from mirgecom.flux import num_flux_lfr
-    return num_flux_lfr(flux_pair.int, flux_pair.ext, cv_pair.int, cv_pair.ext,
-                        lam)
+def inviscid_flux_rusanov(discr, gas_model, state_pair, **kwargs):
+    r"""High-level interface for inviscid facial flux using Rusanov numerical flux.
 
-
-def inviscid_rusanov(discr, gas_model, state_pair):
-    """High-level interface for inviscid facial flux using Rusanov numerical flux."""
-    actx = state_pair.int.array_context
-    normal = thaw(actx, discr.normal(state_pair.dd))
-
-    flux_pair = TracePair(state_pair.dd,
-                           interior=inviscid_flux(state_pair.int)@normal,
-                           exterior=inviscid_flux(state_pair.ext)@normal)
-
-    # This calculates the local maximum eigenvalue of the flux Jacobian
-    # for a single component gas, i.e. the element-local max wavespeed |v| + c.
-    w_int = state_pair.int.speed_of_sound + state_pair.int.speed
-    w_ext = state_pair.ext.speed_of_sound + state_pair.ext.speed
-    # Use "gridspeed" for lamda to make this a Lax-Friedrichs
-    lam = actx.np.maximum(w_int, w_ext)
-
-    cv_pair = TracePair(state_pair.dd,
-                         interior=state_pair.int.cv,
-                         exterior=state_pair.ext.cv)
-    return _lfr_flux(cv_pair, flux_pair, lam)
-
-
-def inviscid_facial_flux(discr, gas_model, state_pair,
-                         numerical_flux_func=inviscid_rusanov, local=False):
-    r"""Return the numerical inviscid flux for the divergence operator.
-
-    Different numerical fluxes may be used through the specificiation of
-    the *numerical_flux_func*. By default, a Rusanov-type flux is calculated
-    as:
+    The Rusanov inviscid numerical flux is calculated as:
 
     .. math::
 
@@ -130,6 +97,16 @@ def inviscid_facial_flux(discr, gas_model, state_pair,
     exterior of the face on which the flux is to be calculated, $\mathbf{F}$ is
     the inviscid fluid flux, $\hat{n}$ is the face normal, and $\lambda$ is the
     *local* maximum fluid wavespeed.
+    """
+    return lfr_flux_driver(discr, state_pair, inviscid_flux)
+
+
+def inviscid_facial_flux(discr, gas_model, state_pair,
+                         numerical_flux_func=inviscid_flux_rusanov, local=False):
+    r"""Return the numerical inviscid flux for the divergence operator.
+
+    Different numerical fluxes may be used through the specificiation of
+    the *numerical_flux_func*. By default, a Rusanov-type flux is used.
 
     Parameters
     ----------
@@ -158,78 +135,8 @@ def inviscid_facial_flux(discr, gas_model, state_pair,
         the face normals as required by the divergence operator for which they
         are being computed.
     """
-    numerical_flux = numerical_flux_func(discr, gas_model, state_pair)
-    return numerical_flux if local else discr.project(state_pair.dd, "all_faces",
-                                                      numerical_flux)
-
-
-def inviscid_facial_divergence_flux(discr, gas_model, state_pair, local=False):
-    r"""Return the flux across a face given the solution on both sides *q_pair*.
-
-    This flux is currently hard-coded to use a Rusanov-type  local Lax-Friedrichs
-    (LFR) numerical flux at element boundaries. The numerical inviscid flux $F^*$ is
-    calculated as:
-
-    .. math::
-
-        \mathbf{F}^{*}_{\mathtt{LFR}} = \frac{1}{2}(\mathbf{F}(q^-)
-        +\mathbf{F}(q^+)) \cdot \hat{n} + \frac{\lambda}{2}(q^{-} - q^{+}),
-
-    where $q^-, q^+$ are the fluid solution state on the interior and the
-    exterior of the face on which the LFR flux is to be calculated, $\mathbf{F}$ is
-    the inviscid fluid flux, $\hat{n}$ is the face normal, and $\lambda$ is the
-    *local* maximum fluid wavespeed.
-
-    Parameters
-    ----------
-    discr: :class:`~grudge.eager.EagerDGDiscretization`
-
-        The discretization collection to use
-
-    state_pair: :class:`~grudge.trace_pair.TracePair`
-
-        Trace pair of :class:`~mirgecom.gas_model.FluidState` for the face upon
-        which the flux calculation is to be performed
-
-    local: bool
-
-        Indicates whether to skip projection of fluxes to "all_faces" or not. If
-        set to *False* (the default), the returned fluxes are projected to
-        "all_faces."  If set to *True*, the returned fluxes are not projected to
-        "all_faces"; remaining instead on the boundary restriction.
-
-    Returns
-    -------
-    :class:`~mirgecom.fluid.ConservedVars`
-
-        A CV object containing the scalar numerical fluxes at the input faces.
-        The returned fluxes are scalar because they've already been dotted with
-        the face normals as required by the divergence operator for which they
-        are being computed.
-    """
-    actx = state_pair.int.array_context
-    flux_pair = TracePair(state_pair.dd,
-                           interior=inviscid_flux(state_pair.int),
-                           exterior=inviscid_flux(state_pair.ext))
-
-    # This calculates the local maximum eigenvalue of the flux Jacobian
-    # for a single component gas, i.e. the element-local max wavespeed |v| + c.
-    w_int = state_pair.int.speed_of_sound + state_pair.int.speed
-    w_ext = state_pair.ext.speed_of_sound + state_pair.ext.speed
-    lam = actx.np.maximum(w_int, w_ext)
-
-    normal = thaw(actx, discr.normal(state_pair.dd))
-    cv_pair = TracePair(state_pair.dd,
-                         interior=state_pair.int.cv,
-                         exterior=state_pair.ext.cv)
-
-    # todo: user-supplied flux routine
-    flux_weak = divergence_flux_lfr(cv_pair, flux_pair, normal=normal, lam=lam)
-
-    if local is False:
-        return discr.project(cv_pair.dd, "all_faces", flux_weak)
-
-    return flux_weak
+    num_flux = numerical_flux_func(discr, gas_model, state_pair)
+    return num_flux if local else discr.project(state_pair.dd, "all_faces", num_flux)
 
 
 def get_inviscid_timestep(discr, state):
