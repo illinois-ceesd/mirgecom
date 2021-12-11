9a0f388d00e406cae57aac26a23528bac143f445
Mike Campbell <mtcampbe@illinois.edu>
2021-12-11 11:49:15 -0600

    switch to custom production branch

--- .ci-support/production-testing-env.sh
+++ .ci-support/production-testing-env.sh
@@ -8,7 +8,7 @@ set -x
 # The production capability may be in a CEESD-local mirgecom branch or in a
 # fork, and is specified through the following settings:
 #
-# export PRODUCTION_BRANCH=""   # The base production branch to be installed by emirge
+export PRODUCTION_BRANCH="production-state-handling-disjoint"   # The base production branch to be installed by emirge
 # export PRODUCTION_FORK=""  # The fork/home of production changes (if any)
 #
 # Multiple production drivers are supported. The user should provide a ':'-delimited
@@ -16,7 +16,7 @@ set -x
 # "fork/repo@branch". The defaults are provided below as an example. Provide custom
 # production drivers in this variable:
 #
-# export PRODUCTION_DRIVERS=""
+export PRODUCTION_DRIVERS="illinois-ceesd/drivers_y1-nozzle@parallel-lazy-state-handling:illinois-ceesd/drivers_y2-isolator@state-handling:illinois-ceesd/drivers_flame1d@state-handling"
 #
 # Example:
 # PRODUCTION_DRIVERS="illinois-ceesd/drivers_y1-nozzle@main:w-hagen/isolator@NS"
