b9e714a01e2a0edae0b3753e83ea392282eee0b9
Michael Campbell <mtcampbe@illinois.edu>
2021-12-12 13:45:49 -0600

    Purge join from tests

--- test/test_operators.py
+++ test/test_operators.py
@@ -217,13 +217,12 @@ def test_grad_operator(actx_factory, dim, order, sym_test_func_factory):
         test_data = test_func(nodes)
         exact_grad = grad_test_func(nodes)
 
-        def inf_norm(x):
-            return actx.to_numpy(discr.norm(x, np.inf))
+        from mirgecom.simutil import componentwise_norms
+        from arraycontext import flatten
 
-        if isinstance(test_data, ConservedVars):
-            err_scale = inf_norm(exact_grad.join())
-        else:
-            err_scale = inf_norm(exact_grad)
+        err_scale = max(flatten(componentwise_norms(discr, exact_grad, np.inf),
+                                actx))
+
         if err_scale <= 1e-16:
             err_scale = 1
 
@@ -236,18 +235,12 @@ def test_grad_operator(actx_factory, dim, order, sym_test_func_factory):
                                          test_data_int_tpair, boundaries)
 
         from mirgecom.operators import grad_operator
-        if isinstance(test_data, ConservedVars):
-            test_grad = make_conserved(
-                dim=dim, q=grad_operator(discr, test_data.join(),
-                                         test_data_flux_bnd.join())
-                )
-            grad_err = inf_norm((test_grad - exact_grad).join())/err_scale
-        else:
-            test_grad = grad_operator(discr, test_data, test_data_flux_bnd)
-            grad_err = inf_norm(test_grad - exact_grad)/err_scale
+        test_grad = grad_operator(discr, test_data, test_data_flux_bnd)
+        grad_err = \
+            max(flatten(componentwise_norms(discr, test_grad - exact_grad, np.inf),
+                        actx)) / err_scale
 
-        print(f"{test_grad=}")
-        eoc.add_data_point(actx.to_numpy(h_max), grad_err)
+        eoc.add_data_point(actx.to_numpy(h_max), actx.to_numpy(grad_err))
 
     assert (
         eoc.order_estimate() >= order - 0.5
