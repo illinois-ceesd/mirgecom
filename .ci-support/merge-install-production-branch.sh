e3725cd0d5cfb267c7deb3b0906977a0c38d5bf9
Andreas Kloeckner <inform@tiker.net>
2022-01-08 12:47:33 -0500

    Production testing workflow: automatically deactivate post-merge

--- .ci-support/merge-install-production-branch.sh
+++ .ci-support/merge-install-production-branch.sh
@@ -12,31 +12,67 @@ set -x
 #
 # PRODUCTION_BRANCH = The production branch (default=production)
 # PRODUCTION_FORK = The production fork (default=illinois-ceesd)
-#
+
 MIRGE_HOME=${1:-"."}
-
-PRODUCTION_BRANCH=${PRODUCTION_BRANCH:-"production"}
-PRODUCTION_FORK=${PRODUCTION_FORK:-"illinois-ceesd"}
-
 echo "MIRGE_HOME=${MIRGE_HOME}"
+
+if [[ "$GITHUB_HEAD_REF" == "$PRODUCTION_PR_MAIN_BRANCH" ]]; then
+  # GITHUB_HEAD_REF is only set for PR builds.
+  # https://docs.github.com/en/actions/learn-github-actions/environment-variables
+  PRODUCTION_BRANCH="production-merge/${PRODUCTION_PR_MAIN_BRANCH}"
+  PRODUCTION_FORK=${PRODUCTION_PR_FORK:-"illinois-ceesd"}
+else
+  PRODUCTION_BRANCH="${PRODUCTION_BRANCH:-"production"}"
+  PRODUCTION_FORK=${PRODUCTION_FORK:-"illinois-ceesd"}
+fi
+
+
 echo "PRODUCTION_FORK=$PRODUCTION_FORK"
 echo "PRODUCTION_BRANCH=$PRODUCTION_BRANCH"
 
 cd "${MIRGE_HOME}" || exit 1
 git status
 
+# $CI is set by Github Actions
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables
 if [[ "$CI" == "true" ]]; then
   # This is needed in order for git to create a merge commit
   git config user.email "ci-runner@ci.machine.com"
   git config user.name "CI Runner"
 fi
 
-# Making a dedicated production remote adds production forks
-git remote add production "https://github.com/${PRODUCTION_FORK}/mirgecom"
-git fetch production
+git fetch -f \
+        "https://github.com/${PRODUCTION_FORK}/mirgecom.git" \
+        "$PRODUCTION_BRANCH":ci-prod-test
 
-# Merge the production branch for testing the production drivers
-git merge "production/${PRODUCTION_BRANCH}" --no-edit
+git fetch -f \
+        "https://github.com/illinois-ceesd/mirgecom.git" \
+        production:ci-prod-upstream
+
+if ! git merge ci-prod-test --no-edit; then
+  echo "*** The branch of this CI run failed to merge with"
+  echo "*** the current production-targeted branch:"
+  echo "*** ${PRODUCTION_BRANCH}"
+  if [[ "$PRODUCTION_BRANCH" == "production" && -n "$GITHUB_HEAD_REF" ]]; then
+    echo "*** You must create a branch off of '$GITHUB_HEAD_REF'"
+    echo "*** that is up-to-date with respect to the 'production' branch in the main"
+    echo "*** mirgecom repo and make it known to the CI in"
+    echo "*** .ci-support/production-testing-env.sh. See the comments there."
+  else
+    echo "*** You must update this branch to be up-to-date with"
+    echo "*** respect to the branch of this test run."
+  fi
+  exit 1
+fi
+
+if ! git merge ci-prod-upstream --no-edit; then
+  echo "*** The current mirgecom branch failed to merge with"
+  echo "*** mirgecom's current production branch."
+  echo "*** You must update this branch to be up-to-date with"
+  echo "*** respect to 'production' in the main mirgecom repository."
+  exit 1
+fi
+
 # Pick up any requirements.txt
 pip install -r requirements.txt
 cd - || exit 1
--- .ci-support/production-testing-env.sh
+++ .ci-support/production-testing-env.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 set -x
+
+# Set this to the mirgecom branch you intend to land in main.  Overwrite
+# whatever value is already here. This branch may live in the main repo or your
+# personal fork, it does not matter.  If what is here does not match the branch
+# name on which you are currently working, then none of this will have any
+# effect.  As a consequence, this change will automatically deactivate itself
+# once merged to main. The production branch must then manually be updated to
+# track the PR production branch.
 #
-# This script is designed to help customize the production environment
-# under which CEESD production capability is tested under a proposed change
-# to illinois-ceesd/mirgecom@main.
+# The corresponding branch intended to land in production must be
+# called "production-merge/$PRODUCTION_PR_MAIN_BRANCH". It may live in the main
+# mirgecom repo or your personal fork, see below.
 #
-# The production capability may be in a CEESD-local mirgecom branch or in a
-# fork, and is specified through the following settings:
-#
-# export PRODUCTION_BRANCH=""   # The base production branch to be installed by emirge
-# export PRODUCTION_FORK=""  # The fork/home of production changes (if any)
-#
+# This branch for production testing must be up-to-date with respect to the
+# production branch in the main mirgecom repo as well as the PR branch. It will
+# only be considered for production testing if the CI is currently testing a
+# branch named $PRODUCTION_PR_MAIN_BRANCH, i.e.
+
+export PRODUCTION_PR_MAIN_BRANCH=drop-vtk-from-env
+
+# If your production branch lives in your personal mirgecom fork,
+# set this to your Github user name. Otherwise, set this to an empty
+# value.
+
+export PRODUCTION_PR_FORK=
+
 # Multiple production drivers are supported. The user should provide a ':'-delimited
 # list of driver locations, where each driver location is of the form:
-# "fork/repo@branch". The defaults are provided below as an example. Provide custom
+# "user/repo@branch". The defaults are provided below as an example. Provide custom
 # production drivers in this variable:
-#
+
 export PRODUCTION_DRIVERS="illinois-ceesd/drivers_y1-nozzle@parallel-lazy-state-handling:illinois-ceesd/drivers_flame1d@state-handling:illinois-ceesd/drivers_y2-isolator@state-handling"
-#
+
 # Example:
 # PRODUCTION_DRIVERS="illinois-ceesd/drivers_y1-nozzle@main:w-hagen/isolator@NS"
