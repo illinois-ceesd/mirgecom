ce76ef6a6b5d7240cbe18f6bb11667d357314ce0
Matthias Diener <mdiener@illinois.edu>
2022-03-22 14:03:13 -0500

    restructure example scripts and add more comments

---
+++ examples/scripts/porter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Generic script to run on multiple GPU ranks.
+# It works by "hiding" GPUs from CUDA that do not
+# correspond to the local rank ID on the nodes, such that
+# only a single GPU
+# Run it like this:
+#   mpirun -n 2 bash run_gpus_generic.sh
+
+set -ex
+
+if [[ -n "$OMPI_COMM_WORLD_NODE_RANK" ]]; then
+    # Open MPI
+    export CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK
+elif [[ -n "MPI_LOCALRANKID" ]]; then
+    # mpich/mvapich
+    export CUDA_VISIBLE_DEVICES=$MPI_LOCALRANKID
+fi
+
+# Assumes POCL
+export PYOPENCL_TEST="port:nv"
+
+python -m mpi4py pulse-mpi.py --lazy
