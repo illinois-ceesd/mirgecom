73064de5937bf7e492a9245e82011985bfb28ee4
Matthew Smith <mjsmith6@illinois.edu>
2022-07-12 09:29:32 -0500

    update create_discretization_collection

--- mirgecom/discretization.py
+++ mirgecom/discretization.py
@@ -38,11 +38,11 @@ logger = logging.getLogger(__name__)
 # we can replace it more easily when we refactor the drivers and
 # examples to use discretization collections, and change it centrally
 # when we want to change it.
-def create_discretization_collection(actx, mesh, order, *, mpi_communicator=None,
-                                     quadrature_order=-1):
+def create_discretization_collection(actx, volume_meshes, order, *,
+        mpi_communicator=None, quadrature_order=-1):
     """Create and return a grudge DG discretization collection."""
     from grudge.dof_desc import DISCR_TAG_BASE, DISCR_TAG_QUAD
-    from grudge.discretization import DiscretizationCollection
+    from grudge.discretization import make_discretization_collection
     from meshmode.discretization.poly_element import (
         QuadratureSimplexGroupFactory,
         PolynomialRecursiveNodesGroupFactory
@@ -51,12 +51,11 @@ def create_discretization_collection(actx, mesh, order, *, mpi_communicator=None
     if quadrature_order < 0:
         quadrature_order = 2*order+1
 
-    return DiscretizationCollection(
-        actx, mesh,
+    return make_discretization_collection(
+        actx, volume_meshes,
         discr_tag_to_group_factory={
             DISCR_TAG_BASE: PolynomialRecursiveNodesGroupFactory(order=order,
                                                                  family="lgl"),
             DISCR_TAG_QUAD: QuadratureSimplexGroupFactory(quadrature_order),
-        },
-        mpi_communicator=mpi_communicator
+        }
     )
