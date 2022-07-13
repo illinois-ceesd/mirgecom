e4a09529009b156390031f67af7a4eec51a79d1c
Michael Campbell <mtcampbe@illinois.edu>
2022-07-13 16:29:25 -0500

    Unduplicate import

--- mirgecom/simutil.py
+++ mirgecom/simutil.py
@@ -77,7 +77,6 @@ from mirgecom.viscous import get_viscous_timestep
 
 from typing import List
 from grudge.discretization import DiscretizationCollection
-from mirgecom.viscous import get_viscous_timestep
 
 
 logger = logging.getLogger(__name__)
