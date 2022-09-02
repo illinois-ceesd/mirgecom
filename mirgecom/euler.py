f08e4b9f98b43e824405ef3f546617d10428235a
Matthew Smith <mjsmith6@illinois.edu>
2022-09-02 12:54:12 -0500

    add user comm tag to operators

--- mirgecom/euler.py
+++ mirgecom/euler.py
@@ -68,7 +68,7 @@ from mirgecom.operators import div_operator
 
 def euler_operator(dcoll, state, gas_model, boundaries, time=0.0,
                    inviscid_numerical_flux_func=inviscid_facial_flux_rusanov,
-                   quadrature_tag=None, operator_states_quad=None):
+                   quadrature_tag=None, comm_tag=None, operator_states_quad=None):
     r"""Compute RHS of the Euler flow equations.
 
     Returns
@@ -107,13 +107,16 @@ def euler_operator(dcoll, state, gas_model, boundaries, time=0.0,
         An optional identifier denoting a particular quadrature
         discretization to use during operator evaluations.
         The default value is *None*.
+
+    comm_tag: Hashable
+        Tag for distributed communication
     """
     dd_quad_vol = DOFDesc("vol", quadrature_tag)
     dd_quad_faces = DOFDesc("all_faces", quadrature_tag)
 
     if operator_states_quad is None:
-        operator_states_quad = make_operator_fluid_states(dcoll, state, gas_model,
-                                                          boundaries, quadrature_tag)
+        operator_states_quad = make_operator_fluid_states(
+            dcoll, state, gas_model, boundaries, quadrature_tag, comm_tag)
 
     volume_state_quad, interior_state_pairs_quad, domain_boundary_states_quad = \
         operator_states_quad
