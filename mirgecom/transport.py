8f717b1acb3696d03df82e7385080296de4b22f1
Tulio <tulioricci@gmail.com>
2022-09-26 11:37:48 -0500

    Remove volume_viscosity in GasTranspVars

--- mirgecom/transport.py
+++ mirgecom/transport.py
@@ -71,14 +71,12 @@ class GasTransportVars:
 
     .. attribute:: bulk_viscosity
     .. attribute:: viscosity
-    .. attribute:: volume_viscosity
     .. attribute:: thermal_conductivity
     .. attribute:: species_diffusivity
     """
 
     bulk_viscosity: np.ndarray
     viscosity: np.ndarray
-    volume_viscosity: np.ndarray
     thermal_conductivity: np.ndarray
     species_diffusivity: np.ndarray
 
@@ -135,7 +133,6 @@ class TransportModel:
         return GasTransportVars(
             bulk_viscosity=self.bulk_viscosity(cv=cv, dv=dv, eos=eos),
             viscosity=self.viscosity(cv=cv, dv=dv, eos=eos),
-            volume_viscosity=self.volume_viscosity(cv=cv, dv=dv, eos=eos),
             thermal_conductivity=self.thermal_conductivity(cv=cv, dv=dv, eos=eos),
             species_diffusivity=self.species_diffusivity(cv=cv, dv=dv, eos=eos)
         )
@@ -268,7 +265,8 @@ class PowerLawTransport(TransportModel):
         """
         return self._beta * dv.temperature**self._n
 
-    def volume_viscosity(self, cv: ConservedVars, dv: GasDependentVars) -> DOFArray:
+    def volume_viscosity(self, cv: ConservedVars, dv: GasDependentVars,
+                         eos: Optional[GasEOS] = None) -> DOFArray:
         r"""Get the 2nd viscosity coefficent, $\lambda$.
 
         In this transport model, the second coefficient of viscosity is defined as:
