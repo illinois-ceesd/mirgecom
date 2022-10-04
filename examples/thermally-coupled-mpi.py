54a05da9805859a6a08c2f16623a6d5f38814df7
Matthew Smith <mjsmith6@illinois.edu>
2022-10-04 15:08:13 -0500

    use energy instead of temperature for wall in coupled_ns_heat_operator

--- examples/thermally-coupled-mpi.py
+++ examples/thermally-coupled-mpi.py
@@ -521,16 +521,18 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
 
     def my_rhs(t, state):
         fluid_state = make_fluid_state(cv=state[0], gas_model=gas_model)
-        wall_temperature = state[1]
-        fluid_rhs, wall_rhs = coupled_ns_heat_operator(
+        wall_energy = wall_density * wall_heat_capacity * state[1]
+        fluid_rhs, wall_energy_rhs = coupled_ns_heat_operator(
             dcoll,
             gas_model,
             dd_vol_fluid, dd_vol_wall,
             fluid_boundaries, wall_boundaries,
             fluid_state,
-            wall_density, wall_heat_capacity, wall_kappa, wall_temperature,
+            wall_density, wall_heat_capacity, wall_kappa, wall_energy,
             time=t, quadrature_tag=quadrature_tag)
-        wall_rhs = wall_time_scale * wall_rhs
+        wall_rhs = (
+            wall_time_scale
+            * wall_energy_rhs/(wall_density * wall_heat_capacity))
         from dataclasses import replace
         fluid_rhs = replace(
             fluid_rhs,
