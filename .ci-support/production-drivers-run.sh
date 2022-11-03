197fc0216df1f0167b5365af59880813b48ff05a
Michael Campbell <mtcampbe@illinois.edu>
2022-11-03 01:13:53 -0500

    Test prod on porter

--- .ci-support/production-drivers-run.sh
+++ .ci-support/production-drivers-run.sh
@@ -7,10 +7,39 @@ set -x
 # a test driver in "production_driver_*/smoke_test/driver.py".
 DRIVERS_HOME=${1:-"."}
 cd ${DRIVERS_HOME}
+
+mpi_exec="mpiexec"
+mpi_launcher=""
+
+if [[ $(hostname) == "porter" ]]; then
+    rm -rf run_gpus_generic.sh
+    cat <<EOF > run_gpus_generic.sh
+
+    if [[ -n "\$OMPI_COMM_WORLD_NODE_RANK" ]]; then
+        # Open MPI
+        export CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK
+    elif [[ -n "\$MPI_LOCALRANKID" ]]; then
+        # mpich/mvapich
+        export CUDA_VISIBLE_DEVICES=$MPI_LOCALRANKID
+    fi
+EOF
+    chmod +x run_gpus_generic.sh
+    mpi_launcher="bash ../../run_gpus_generic.sh"
+
+    # Assumes POCL
+    export PYOPENCL_TEST="port:nv"
+    export PYOPENCL_CTX="port:nv"
+elif [[ $(hostname) == "lassen"* ]]; then
+    export PYOPENCL_CTX="port:tesla"
+    export PYOPENCL_TEST="port:tesla"
+    export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"
+    mpi_exec="jsrun -g 1 -a 1"
+fi
+
 for production_driver in $(ls | grep "production_driver_");
 do
     cd "$production_driver"/smoke_test
-    mpiexec -n 2 python -m mpi4py ./driver.py -i run_params.yaml --log --lazy
+    ${mpi_exec} -n 2 ${mpi_launcher} python -m mpi4py ./driver.py -i run_params.yaml --log --lazy
     cd ../../
 done
 cd -
