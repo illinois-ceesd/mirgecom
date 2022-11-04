df0a7e707532dd199249d2fe27bb54955263114c
Michael Campbell <mtcampbe@illinois.edu>
2022-11-04 15:57:17 -0500

    udpate scripts

--- scripts/mirge-testing-env.sh
+++ scripts/mirge-testing-env.sh
@@ -3,18 +3,19 @@
 MIRGE_HOME=${1:-"."}
 cd ${MIRGE_HOME}
 MIRGE_HOME=$(pwd)
+cd -
 
 MIRGE_PARALLEL_SPAWNER=""
 MIRGE_MPI_EXEC="mpiexec"
 XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"
 
 if [[ $(hostname) == "porter" ]]; then
-    MIRGE_PARALLEL_SPAWNER="bash ${MIRGE_HOME}/scripts/porter_parallel_spawner.sh"
+    MIRGE_PARALLEL_SPAWNER="bash ${MIRGE_HOME}/scripts/porter-parallel-spawner.sh"
     PYOPENCL_TEST="port:nv"
     PYOPENCL_CTX="port:nv"
 
 elif [[ $(hostname) == "lassen"* ]]; then
-    MIRGE_PARALLEL_SPAWNER="bash ${MIRGE_HOME}/scripts/lassen_parallel_spawner.sh"
+    MIRGE_PARALLEL_SPAWNER="bash ${MIRGE_HOME}/scripts/lassen-parallel-spawner.sh"
     PYOPENCL_TEST="port:tesla"
     PYOPENCL_CTX="port:tesla"
     MIRGE_MPI_EXEC="jsrun -g 1 -a 1"    
@@ -26,4 +27,3 @@ export XDG_CACHE_HOME
 export PYOPENCL_TEST
 export PYOPENCL_CTX
 
-cd -
