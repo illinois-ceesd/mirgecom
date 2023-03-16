cd7bfb9bf94e94338c0f8dd3027e8b957646f268
Matthias Diener <mdiener@illinois.edu>
2023-03-16 10:28:13 -0500

    mpi.py: add comments

--- mirgecom/mpi.py
+++ mirgecom/mpi.py
@@ -166,10 +166,13 @@ def mpi_entry_point(func) -> Callable:
     """
     @wraps(func)
     def wrapped_func(*args, **kwargs) -> None:
+        # We enforce this so that an exception raised on one rank terminates all ranks.
         if "mpi4py.run" not in sys.modules:
             raise RuntimeError("Must run MPI scripts via mpi4py (i.e., 'python -m "
                         "mpi4py <args>').")
 
+        # We enforce this so that we can work around certain interoperability issues,
+        # including the hwloc/mprobe ones below.
         if "mpi4py.MPI" in sys.modules:
             raise RuntimeError("mpi4py.MPI imported before designated MPI entry "
                         "point. Check for prior imports.")
@@ -181,7 +184,7 @@ def mpi_entry_point(func) -> Callable:
         cl.get_platforms()
 
         # Avoid https://github.com/illinois-ceesd/mirgecom/issues/132 on
-        # some MPI runtimes.
+        # some MPI runtimes. This must be set *before* the first import of mpi4py.MPI.
         import mpi4py
         mpi4py.rc.recv_mprobe = False
 
