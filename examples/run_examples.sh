00ca34521af7fd7984c54501f67e7fc4f9fbab54
Matthias Diener <mdiener@illinois.edu>
2023-04-08 14:17:08 -0500

    No more -lazy symlinks in examples/

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -14,6 +14,22 @@ echo "*** Running examples in $examples_dir ..."
 failed_examples=""
 succeeded_examples=""
 
+function check_return_code()
+{
+    local return_code=$1
+    local example=$2
+    if [[ $return_code -eq 0 ]]
+    then
+        ((numsuccess=numsuccess+1))
+        echo "*** Example $example succeeded."
+        succeeded_examples="$succeeded_examples $example"
+    else
+        ((numfail=numfail+1))
+        echo "*** Example $example failed."
+        failed_examples="$failed_examples $example"
+    fi
+}
+
 if [[ -z "${MIRGE_PARALLEL_SPAWNER:-}" ]];then
     . ${examples_dir}/scripts/mirge-testing-env.sh ${examples_dir}/..
 fi
@@ -34,44 +50,31 @@ for example in $examples
 do
     date
     printf "***\n***\n"
-    if [[ "$example" == *"-mpi-lazy.py" ]]
-    then
-        echo "*** Running parallel lazy example (2 ranks): $example"
-        set -x
-        ${mpi_exec} -n 2 $mpi_launcher python -u -O -m mpi4py ${example} --lazy
-        example_return_code=$?
-        set +x
-    elif [[ "$example" == *"-mpi.py" ]]; then
-        echo "*** Running parallel example (2 ranks): $example"
+    if [[ "$example" == *"-mpi.py" ]]; then
+        echo "*** Running distributed eager example (2 ranks): $example"
         set -x
         ${mpi_exec} -n 2 $mpi_launcher python -u -O -m mpi4py ${example}
-        example_return_code=$?
+        check_return_code $? ${example}_eager
         set +x
-    elif [[ "$example" == *"-lazy.py" ]]; then
+        echo "*** Running distributed lazy example (2 ranks): $example"
+        set -x
+        ${mpi_exec} -n 2 $mpi_launcher python -u -O -m mpi4py ${example} --lazy
+        check_return_code $? ${example}_lazy
+        set +x
+    else
+        echo "*** Running serial eager example: $example"
+        set -x
+        python -u -O ${example}
+        check_return_code $? ${example}_eager
+        set +x
         echo "*** Running serial lazy example: $example"
         set -x
         python -u -O ${example} --lazy
-        example_return_code=$?
-        set +x
-    else
-        echo "*** Running serial example: $example"
-        set -x
-        python -u -O ${example}
-        example_return_code=$?
+        check_return_code $? ${example}_lazy
         set +x
     fi
     date
     printf "***\n"
-    if [[ $example_return_code -eq 0 ]]
-    then
-        ((numsuccess=numsuccess+1))
-        echo "*** Example $example succeeded."
-        succeeded_examples="$succeeded_examples $example"
-    else
-        ((numfail=numfail+1))
-        echo "*** Example $example failed."
-        failed_examples="$failed_examples $example"
-    fi
     rm -rf *vtu *sqlite *pkl *-journal restart_data
 done
 ((numtests=numsuccess+numfail))
