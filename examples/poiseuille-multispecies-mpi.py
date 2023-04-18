313f0e193d05b6dc14052e09fa6a81f1e8990f63
Michael Campbell <mtcampbe@illinois.edu>
2023-04-17 20:23:58 -0500

    Update for production features

--- examples/poiseuille-multispecies-mpi.py
+++ examples/poiseuille-multispecies-mpi.py
@@ -1,4 +1,4 @@
-"""Demonstrate a planar Poiseuille flow example."""
+"""Demonstrate a planar Poiseuille flow example with multispecies."""
 
 __copyright__ = """
 Copyright (C) 2020 University of Illinois Board of Trustees
@@ -31,10 +31,10 @@ from functools import partial
 
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 
+from grudge.eager import EagerDGDiscretization
 from grudge.shortcuts import make_visualizer
-from grudge.dof_desc import BoundaryDomainTag, DISCR_TAG_QUAD
+from grudge.dof_desc import BoundaryDomainTag
 
-from mirgecom.discretization import create_discretization_collection
 from mirgecom.fluid import make_conserved
 from mirgecom.navierstokes import ns_operator
 from mirgecom.simutil import get_sim_timestep
@@ -45,17 +45,17 @@ from mirgecom.integrators import rk4_step
 from mirgecom.steppers import advance_state
 from mirgecom.boundary import (
     PrescribedFluidBoundary,
-    AdiabaticNoslipWallBoundary
+    IsothermalWallBoundary
 )
 from mirgecom.transport import SimpleTransport
-from mirgecom.eos import IdealSingleGas
+from mirgecom.eos import IdealSingleGas  # , PyrometheusMixture
 from mirgecom.gas_model import GasModel, make_fluid_state
 from logpyle import IntervalTimer, set_dt
 from mirgecom.euler import extract_vars_for_logging, units_for_logging
 from mirgecom.logging_quantities import (
     initialize_logmgr,
     logmgr_add_many_discretization_quantities,
-    logmgr_add_cl_device_info,
+    logmgr_add_device_name,
     logmgr_add_device_memory_usage,
     set_sim_state
 )
@@ -116,7 +116,7 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
     alloc = get_reasonable_memory_pool(cl_ctx, queue)
 
     if lazy:
-        actx = actx_class(comm, queue, mpi_base_tag=12000, allocator=alloc)
+        actx = actx_class(comm, queue, allocator=alloc, mpi_base_tag=12000)
     else:
         actx = actx_class(comm, queue, allocator=alloc, force_device_scalars=True)
 
@@ -124,25 +124,32 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
     timestepper = rk4_step
     t_final = 1e-7
     current_cfl = 0.05
-    current_dt = 1e-10
+    current_dt = 1e-8
     current_t = 0
-    constant_cfl = True
+    constant_cfl = False
     current_step = 0
 
     # some i/o frequencies
-    nstatus = 1
-    nviz = 1
-    nrestart = 100
-    nhealth = 1
+    nstatus = 100
+    nviz = 10
+    nrestart = 1000
+    nhealth = 100
 
     # some geometry setup
     dim = 2
     if dim != 2:
         raise ValueError("This example must be run with dim = 2.")
+    x_ch = 1e-4
     left_boundary_location = 0
-    right_boundary_location = 0.1
+    right_boundary_location = 0.02
     ybottom = 0.
-    ytop = .02
+    ytop = .002
+    xlen = right_boundary_location - left_boundary_location
+    ylen = ytop - ybottom
+    n_refine = 1
+    npts_x = n_refine*int(xlen / x_ch)
+    npts_y = n_refine*int(ylen / x_ch)
+
     rst_path = "restart_data/"
     rst_pattern = (
         rst_path + "{cname}-{step:04d}-{rank:04d}.pkl"
@@ -157,9 +164,6 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
         global_nelements = restart_data["global_nelements"]
         assert restart_data["nparts"] == nparts
     else:  # generate the grid from scratch
-        n_refine = 5
-        npts_x = 10 * n_refine
-        npts_y = 6 * n_refine
         npts_axis = (npts_x, npts_y)
         box_ll = (left_boundary_location, ybottom)
         box_ur = (right_boundary_location, ytop)
@@ -169,8 +173,20 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
                                                                     generate_mesh)
         local_nelements = local_mesh.nelements
 
+    from grudge.dof_desc import DISCR_TAG_BASE, DISCR_TAG_QUAD
+    from meshmode.discretization.poly_element import \
+        default_simplex_group_factory, QuadratureSimplexGroupFactory
+
     order = 2
-    dcoll = create_discretization_collection(actx, local_mesh, order=order)
+    dcoll = EagerDGDiscretization(
+        actx, local_mesh,
+        discr_tag_to_group_factory={
+            DISCR_TAG_BASE: default_simplex_group_factory(
+                base_dim=local_mesh.dim, order=order),
+            DISCR_TAG_QUAD: QuadratureSimplexGroupFactory(2*order + 1)
+        },
+        mpi_communicator=comm
+    )
     nodes = actx.thaw(dcoll.nodes())
 
     if use_overintegration:
@@ -179,7 +195,7 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
         quadrature_tag = None
 
     if logmgr:
-        logmgr_add_cl_device_info(logmgr, queue)
+        logmgr_add_device_name(logmgr, queue)
         logmgr_add_device_memory_usage(logmgr, queue)
         logmgr_add_many_discretization_quantities(logmgr, dcoll, dim,
                              extract_vars_for_logging, units_for_logging)
@@ -199,43 +215,60 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
         logmgr.add_quantity(vis_timer)
 
     base_pressure = 100000.0
-    pressure_ratio = 1.001
-    mu = 1.0
+    pressure_ratio = 1.08
+    # MikeA: mu=5e-4, spec_d=1e-4, dt=1e-8, kappa=1e-5
+    mu = 5e-4
+    kappa = 0.
+    nspecies = 2
+    species_diffusivity = 1e-5 * np.ones(nspecies)
+    xlen = right_boundary_location - left_boundary_location
+    ylen = ytop - ybottom
 
     def poiseuille_2d(x_vec, eos, cv=None, **kwargs):
         y = x_vec[1]
         x = x_vec[0]
+        # zeros = 0*x
+        ones = 0*x + 1.
         x0 = left_boundary_location
         xmax = right_boundary_location
         xlen = xmax - x0
+        wgt1 = actx.np.less(x, xlen/2)
+        wgt2 = 1 - wgt1
+        # xcor = x*ones
+        # leno2 = xlen/2*ones
         p_low = base_pressure
         p_hi = pressure_ratio*base_pressure
         dp = p_hi - p_low
         dpdx = dp/xlen
         h = ytop - ybottom
         u_x = dpdx*y*(h - y)/(2*mu)
+        print(f"flow speed = {dpdx*h*h/(8*mu)}")
         p_x = p_hi - dpdx*x
-        rho = 1.0
+        rho = 1.0*ones
         mass = 0*x + rho
         u_y = 0*x
         velocity = make_obj_array([u_x, u_y])
         ke = .5*np.dot(velocity, velocity)*mass
         gamma = eos.gamma()
+        rho_y = rho * make_obj_array([1.0/nspecies for _ in range(nspecies)])
+        rho_y[0] = wgt1*rho_y[0]
+        rho_y[1] = wgt2*rho_y[1]
         if cv is not None:
-            mass = cv.mass
-            vel = cv.velocity
-            ke = .5*np.dot(vel, vel)*mass
+            rho_y = wgt1*rho_y + wgt2*mass*cv.species_mass_fractions
 
         rho_e = p_x/(gamma-1) + ke
         return make_conserved(2, mass=mass, energy=rho_e,
-                              momentum=mass*velocity)
+                              momentum=mass*velocity,
+                              species_mass=rho_y)
 
     initializer = poiseuille_2d
     gas_model = GasModel(eos=IdealSingleGas(),
-                         transport=SimpleTransport(viscosity=mu))
+                         transport=SimpleTransport(
+                             viscosity=mu, thermal_conductivity=kappa,
+                             species_diffusivity=species_diffusivity))
     exact = initializer(x_vec=nodes, eos=gas_model.eos)
 
-    def _boundary_solution(dcoll, dd_bdry, gas_model, state_minus, **kwargs):
+    def _exact_boundary_solution(dcoll, dd_bdry, gas_model, state_minus, **kwargs):
         actx = state_minus.array_context
         bnd_discr = dcoll.discr_from_dd(dd_bdry)
         nodes = actx.thaw(bnd_discr.nodes())
@@ -244,13 +277,13 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
 
     boundaries = {
         BoundaryDomainTag("-1"):
-            PrescribedFluidBoundary(
-                boundary_state_func=_boundary_solution),
+            PrescribedFluidBoundary(boundary_state_func=_exact_boundary_solution),
         BoundaryDomainTag("+1"):
-            PrescribedFluidBoundary(
-                boundary_state_func=_boundary_solution),
-        BoundaryDomainTag("-2"): AdiabaticNoslipWallBoundary(),
-        BoundaryDomainTag("+2"): AdiabaticNoslipWallBoundary()}
+            PrescribedFluidBoundary(boundary_state_func=_exact_boundary_solution),
+        BoundaryDomainTag("-2"):
+            IsothermalWallBoundary(wall_temperature=348.5),
+        BoundaryDomainTag("+2"):
+            IsothermalWallBoundary(wall_temperature=348.5)}
 
     if rst_filename:
         current_t = restart_data["t"]
@@ -262,8 +295,6 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
     else:
         # Set the current state from time 0
         current_cv = exact
-
-    current_state = make_fluid_state(cv=current_cv, gas_model=gas_model)
 
     vis_timer = None
 
@@ -309,7 +340,7 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
 
         from mirgecom.simutil import write_visfile
         write_visfile(dcoll, viz_fields, visualizer, vizname=casename,
-                      step=step, t=t, overwrite=True, comm=comm)
+                      step=step, t=t, overwrite=True)
 
     def my_write_restart(step, t, state):
         rst_fname = rst_pattern.format(cname=casename, step=step, rank=rank)
@@ -335,7 +366,7 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
 
         if global_reduce(check_range_local(dcoll, "vol", dv.pressure, 9.999e4,
                                            1.00101e5), op="lor"):
-            health_error = True
+            health_error = False
             from grudge.op import nodal_max, nodal_min
             p_min = actx.to_numpy(nodal_min(dcoll, "vol", dv.pressure))
             p_max = actx.to_numpy(nodal_max(dcoll, "vol", dv.pressure))
@@ -347,15 +378,15 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
 
         if global_reduce(check_range_local(dcoll, "vol", dv.temperature, 348, 350),
                          op="lor"):
-            health_error = True
+            health_error = False
             from grudge.op import nodal_max, nodal_min
             t_min = actx.to_numpy(nodal_min(dcoll, "vol", dv.temperature))
             t_max = actx.to_numpy(nodal_max(dcoll, "vol", dv.temperature))
             logger.info(f"Temperature range violation ({t_min=}, {t_max=})")
 
-        exittol = .1
+        exittol = 1e7
         if max(component_errors) > exittol:
-            health_error = True
+            health_error = False
             if rank == 0:
                 logger.info("Solution diverged from exact soln.")
 
@@ -420,11 +451,25 @@ def main(ctx_factory=cl.create_some_context, use_logmgr=True,
             logmgr.tick_after()
         return state, dt
 
+    orig = np.zeros(shape=(dim,))
+    orig[0] = 2*xlen/5.
+    orig[1] = 7*ylen/10.
+
+    def acoustic_pulse(time, fluid_cv, gas_model):
+        from mirgecom.initializers import AcousticPulse
+        acoustic_pulse = AcousticPulse(dim=dim, amplitude=5000.0, width=.0001,
+                                       center=orig)
+        # return fluid_cv
+        return acoustic_pulse(nodes, cv=fluid_cv, eos=gas_model.eos)
+
     def my_rhs(t, state):
         fluid_state = make_fluid_state(state, gas_model)
         return ns_operator(dcoll, gas_model=gas_model, boundaries=boundaries,
                            state=fluid_state, time=t,
                            quadrature_tag=quadrature_tag)
+
+    current_state = make_fluid_state(
+        cv=acoustic_pulse(current_t, current_cv, gas_model), gas_model=gas_model)
 
     current_dt = get_sim_timestep(dcoll, current_state, current_t, current_dt,
                                   current_cfl, t_final, constant_cfl)
@@ -477,8 +522,8 @@ if __name__ == "__main__":
     parser.add_argument("--restart_file", help="root name of restart file")
     parser.add_argument("--casename", help="casename to use for i/o")
     args = parser.parse_args()
-
     lazy = args.lazy
+
     if args.profiling:
         if lazy:
             raise ValueError("Can't use lazy and profiling together.")
