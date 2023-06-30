d01cb35e177a38c0c517c6c2cca00c57f3fb9194
aakankshbhat <80215341+aakankshbhat@users.noreply.github.com>
2023-06-30 22:12:34 +0530

    Update simutil.py

--- mirgecom/simutil.py
+++ mirgecom/simutil.py
@@ -92,6 +92,24 @@ def get_number_of_tetrahedron_nodes(dim, order, include_faces=False):
         nnodes = nnodes + (dim+1)*get_number_of_tetrahedron_nodes(dim-1, order)
     return nnodes
 
+def get_box_mesh(dim,a,b,n, t=None, periodic=None):
+    if periodic is None:
+        periodic = (False,)*dim
+    if np.isscalar(a):
+        a = (a,)*dim
+    if np.isscalar(b):
+        b = (b,)*dim
+    if np.isscalar(n):
+        n = (n,)*dim
+
+    dim_names = ["x","y","z"]
+    bttf = {}
+    for i in range(dim):
+        bttf["-"+str(i+1)] = ["-"+dim_names[i]]
+        bttf["+"+str(i+1)] = ["+"+dim_names[i]]
+    from meshmode.mesh.generation import generate_regular_rect_mesh as gen
+    return gen(a=a, b=b, n=n, boundary_tag_to_face=bttf, mesh_type=t,periodic=periodic)
+
 
 def check_step(step, interval):
     """
