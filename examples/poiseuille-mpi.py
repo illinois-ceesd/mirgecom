cbf44d3c7bf9e08da26ae870e74565730a78f3fb
aakankshbhat <80215341+aakankshbhat@users.noreply.github.com>
2023-07-03 15:18:30 +0530

    import get_box_mesh instead of defining it

--- examples/poiseuille-mpi.py
+++ examples/poiseuille-mpi.py
@@ -37,7 +37,10 @@ from grudge.dof_desc import BoundaryDomainTag, DISCR_TAG_QUAD
 from mirgecom.discretization import create_discretization_collection
 from mirgecom.fluid import make_conserved
 from mirgecom.navierstokes import ns_operator
-from mirgecom.simutil import get_sim_timestep
+from mirgecom.simutil import (
+    get_sim_timestep
+    get_box_mesh
+)
 
 from mirgecom.io import make_init_message
 from mirgecom.mpi import mpi_entry_point
@@ -68,17 +71,6 @@ class MyRuntimeError(RuntimeError):
     """Simple exception to kill the simulation."""
 
     pass
-
-
-# Box grid generator widget lifted from @majosm and slightly bent
-def _get_box_mesh(dim, a, b, n, t=None):
-    dim_names = ["x", "y", "z"]
-    bttf = {}
-    for i in range(dim):
-        bttf["-"+str(i+1)] = ["-"+dim_names[i]]
-        bttf["+"+str(i+1)] = ["+"+dim_names[i]]
-    from meshmode.mesh.generation import generate_regular_rect_mesh as gen
-    return gen(a=a, b=b, n=n, boundary_tag_to_face=bttf, mesh_type=t)
 
 
 @mpi_entry_point
