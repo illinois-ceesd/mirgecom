dd58b52393af1d3c131ec91a846707732b69010d
aakankshbhat <80215341+aakankshbhat@users.noreply.github.com>
2023-07-15 21:27:15 +0530

    Whitespace changes

--- mirgecom/simutil.py
+++ mirgecom/simutil.py
@@ -119,7 +119,7 @@ def get_box_mesh(dim, a, b, n, t=None, periodic=None):
         bttf["+"+str(i+1)] = ["+"+dim_names[i]]
     from meshmode.mesh.generation import generate_regular_rect_mesh as gen
     return gen(a=a, b=b, n=n, 
-               boundary_tag_to_face=bttf,mesh_type=t,periodic=periodic)
+               boundary_tag_to_face=bttf, mesh_type=t, periodic=periodic)
 
 
 def check_step(step, interval):
