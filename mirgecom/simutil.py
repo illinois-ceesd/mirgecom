fb453c76b0638e323894acb2d56ae01c883e08cd
aakankshbhat <80215341+aakankshbhat@users.noreply.github.com>
2023-07-17 15:54:22 +0530

    Whitespace and blank line corrections

--- mirgecom/simutil.py
+++ mirgecom/simutil.py
@@ -98,9 +98,11 @@ def get_box_mesh(dim, a, b, n, t=None, periodic=None):
     Create a rectangular "box" like mesh with tagged boundary faces.
 
     Ensure parameters needed for generate_regular_rect_mesh are satisfied
-    a,b,n,periodic are tuples of dimension = dim and are converted if not in this format
+    a,b,n,periodic are tuples of dimension = dim 
+    converted if not in this format
 
-    bttf is a list of all dimensions in the problem in both positive and negative directions (x,-x...)
+    bttf is a list of all dimensions in the problem
+    in both positive and negative directions (x,-x...)
     depends on value of dim
     """
     if periodic is None:
@@ -111,15 +113,15 @@ def get_box_mesh(dim, a, b, n, t=None, periodic=None):
         b = (b,)*dim
     if np.isscalar(n):
         n = (n,)*dim
-
     dim_names = ["x", "y", "z"]
     bttf = {}
     for i in range(dim):
         bttf["-"+str(i+1)] = ["-"+dim_names[i]]
         bttf["+"+str(i+1)] = ["+"+dim_names[i]]
     from meshmode.mesh.generation import generate_regular_rect_mesh as gen
-    return gen(a=a, b=b, n=n, 
-               boundary_tag_to_face=bttf, mesh_type=t, periodic=periodic)
+    return gen(a=a, b=b, n=n,
+               boundary_tag_to_face=bttf,
+               mesh_type=t, periodic=periodic)
 
 
 def check_step(step, interval):
