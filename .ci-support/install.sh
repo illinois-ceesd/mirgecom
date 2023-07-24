1966968b3704e9682194c59011d74c5e6a49f769
Matthias Diener <mdiener@illinois.edu>
2023-07-24 11:38:04 -0500

    use mambaforge

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -2,13 +2,14 @@
 
 set -ex
 
+myos=$(uname)
+myarch=$(uname -m)
+
 if [[ "$(uname)" = "Darwin" ]]; then
-    PLATFORM=MacOSX
     brew update
     brew install open-mpi
     brew install octave
 else
-    PLATFORM=Linux
     if ! command -v mpicc &> /dev/null ;then
         sudo apt-get update
         sudo apt-get -y install libopenmpi-dev openmpi-bin moreutils
@@ -19,16 +20,16 @@ else
 fi
 
 MINIFORGE_INSTALL_DIR=.miniforge3
-MINIFORGE_INSTALL_SH=Miniforge3-$PLATFORM-x86_64.sh
+MINIFORGE_INSTALL_SH=Mambaforge-$myos-$myarch.sh
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/$MINIFORGE_INSTALL_SH"
 rm -Rf "$MINIFORGE_INSTALL_DIR"
-bash "$MINIFORGE_INSTALL_SH" -b -p "$MINIFORGE_INSTALL_DIR" | ts
+bash "$MINIFORGE_INSTALL_SH" -b -p "$MINIFORGE_INSTALL_DIR"
 
 # Temporarily disabled to get CI unstuck
 #PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update conda --yes --quiet
 #PATH="$MINIFORGE_INSTALL_DIR/bin/:$PATH" conda update --all --yes --quiet
 
-$MINIFORGE_INSTALL_DIR/bin/conda env create --file conda-env.yml --name testing | ts
+$MINIFORGE_INSTALL_DIR/bin/mamba env create --file conda-env.yml --name testing | ts
 
 . "$MINIFORGE_INSTALL_DIR/bin/activate" testing
 conda list
