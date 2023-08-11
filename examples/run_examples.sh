38dccf7915f821909be77d7ea56ede73ba9188d6
Mike Campbell <mtcampbe@illinois.edu>
2023-08-11 10:46:45 -0500

    Track and summarize test results, allow selected examples, use reasonable tolerances

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-set -ex
+# This option is undesired here.  We want to make sure to try all the examples,
+# even if some fail.
+# set -e
+# I'd prefer to set this judiciously to have clean-looking output in the CI logs
+# set -x
 set -o nounset
-
-rm -f *.vtu *.pvtu
 
 origin=$(pwd)
 examples_dir=${1-$origin}
+shift
+
+# This bit will let the user specify which
+# examples to run, default to run them all.
+if [[ "$#" -eq 0 ]]; then # use all py files
+    example_list=(*.py) 
+else # Run specific examples if given by user
+    example_list=("$@")
+    # Add the .py extension to each example in the list.
+    for i in "${!example_list[@]}"; do
+        example_list[$i]="${example_list[$i]}.py"
+    done
+fi
+
+
+num_failed_examples=0
+num_successful_examples=0
+failed_examples=""
+succeeded_examples=""
+succeeded_tests=""
+failed_tests=""
 
 date
 echo "*** Running examples in $examples_dir ..."
@@ -23,44 +46,195 @@ cd $examples_dir
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
-for example in *.py
+for example in "${example_list[@]}"
 do
-    if [[ $example == "wave-nompi.py" ]]; then
-        echo "Skipping $example"
-        continue
+    example_name="${example%.py}"
+
+    # FIXME: Let's still test the serial examples
+    if [[ $example_name == *"nompi"* ]]; then
+        echo "*** Skipping serial example (for now): $example_name"
+        continue        
     fi
 
-    TOL_LAZY=1e-04
-    TOL_NUMPY=1e-05
-
-    if [[ $example == "thermally-coupled.py" ]]; then
-        echo "Setting tolerance=1 for $example"
-        TOL_LAZY=1
-        TOL_NUMPY=1
-    fi
-
+    # FIXME: These tolerance tweaks render the comparisons ineffective
+    # for the intended purpose 
+    # TOL_LAZY=1e-04
+    # TOL_NUMPY=1e-05
+    # if [[ $example == "thermally-coupled.py" ]]; then
+    #    echo "Setting tolerance=1 for $example"
+    #    TOL_LAZY=1
+    #    TOL_NUMPY=1
+    # fi
+    # SHOULD BE
+    # TOL_LAZY=1e-12
+    # TOL_NUMPY=1e-12
+    #
+    # TEMPORARILY SET HIGH TOLs
+    TOL_LAZY=1e-9
+    TOL_NUMPY=1e-9
 
     date
     printf "***\n***\n"
 
-    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${example}-eager
-    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${example}-lazy --lazy
-    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${example}-numpy --numpy
+    declare -A test_results
 
-    # Note: not all examples produce vtu files, for these the accuracy test won't be
+    # Run Eager, Lazy, and Numpy contexts
+    test_name="${example_name}_eager"
+    echo "**** Running $test_name"
+    set -x
+    rm -rf ${test_name}*vtu viz_data/${test_name}*vtu
+    set +x
+
+    set -x
+    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${test_name}
+    test_return_code=$?
+    set +x
+    test_results["${test_name}"]=$test_return_code
+    date
+
+    test_name="${example_name}_lazy"
+    echo "**** Running $test_name"
+    set -x
+    rm -rf ${test_name}*vtu viz_data/${test_name}*vtu
+    set +x
+
+    set -x
+    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${test_name} --lazy
+    test_return_code=$?
+    set +x
+    test_results["$test_name"]=$test_return_code
+    date
+
+    test_name="${example_name}_numpy"
+    echo "**** Running $test_name"
+
+    set -x
+    rm -rf ${test_name}*vtu viz_data/${test_name}*vtu
+    set +x
+
+    set -x
+    ${mpi_exec} -n 2 $mpi_launcher python -m mpi4py $example --casename ${test_name} --numpy
+    test_return_code=$?
+    set +x
+    test_results["$test_name"]=$test_return_code
+    date
+
+    # FIXME: not all examples produce vtu files, for these the accuracy test won't be
     # run.
-    for vizfile in $(ls ${example}-eager-*.vtu); do
-        lazy_vizfile=$(echo ${vizfile/eager/lazy})
-        python ${examples_dir}/../bin/mirgecompare.py --tolerance $TOL_LAZY ${vizfile} ${lazy_vizfile}
-        numpy_vizfile=$(echo ${vizfile/eager/numpy})
-        python ${examples_dir}/../bin/mirgecompare.py --tolerance $TOL_NUMPY ${vizfile} ${numpy_vizfile}
+    # - may be fixed now, we look even in viz_data.
+    echo "**** Accuracy comparison for $example_name."
+    lazy_comparison_result=0
+    numpy_comparison_result=0
+    nlazy_compare=0
+    nnumpy_compare=0
+    for eager_vizfile in ${example_name}_eager-*.vtu viz_data/${example_name}_eager-*.vtu; do
+ 
+        if [[ -f ${eager_vizfile} ]]; then
+            lazy_vizfile=$(echo ${eager_vizfile/eager/lazy})
+            if [[ -f ${lazy_vizfile} ]]; then
+                echo "***** comparing lazy results..."
+                python ${examples_dir}/../bin/mirgecompare.py --tolerance $TOL_LAZY ${eager_vizfile} ${lazy_vizfile}
+                lazy_compare_return_code=$?
+                lazy_comparison_result=$((lazy_comparison_result + lazy_compare_return_code))
+                ((nlazy_compare++))
+            fi
+
+            numpy_vizfile=$(echo ${eager_vizfile/eager/numpy})
+            if [[ -f ${numpy_vizfile} ]]; then
+                echo "***** comparing numpy results..."
+                python ${examples_dir}/../bin/mirgecompare.py --tolerance $TOL_NUMPY ${eager_vizfile} ${numpy_vizfile}
+                numpy_compare_return_code=$?
+                numpy_comparison_result=$((numpy_comparison_result + numpy_compare_return_code))
+                ((nnumpy_compare++))
+            fi
+        fi
     done
 
-    date
-    printf "***\n"
-    # FIXME: This could delete data from other runs
-    # rm -rf *vtu *sqlite *pkl *-journal restart_data
-done
+    # Save any comparison results (if they were done)
+    if [[ "$nlazy_compare" -gt 0 ]]; then
+        test_results["${example_name}_lazy_comparison"]=$lazy_comparison_result
+    fi
 
+    if [[ "$nnumpy_compare" -gt 0 ]]; then
+        test_results["${example_name}_numpy_comparison"]=$numpy_comparison_result
+    fi
+
+    gen_test_names=("eager" "lazy" "numpy" "lazy_comparison" "numpy_comparison")
+
+    example_test_result=0
+    num_ex_success=0
+    num_ex_failed=0
+    example_succeeded_tests=""
+    example_failed_tests=""
+
+    # Track/report the suite of tests for each example
+    echo "${example_name} testing results:"
+    for example_test_name in "${gen_test_names[@]}"; do
+        test_name="${example_name}_${example_test_name}"
+        # if [[ -v test_results["$test_name"] ]]; then
+        if [[ ${test_results["$test_name"]+_} ]]; then
+            printf "${test_name}: "
+            if [[ ${test_results["$test_name"]} -eq 0 ]]
+            then
+                example_succeeded_tests="${example_succeeded_tests} ${test_name}"
+                ((num_ex_success++))
+                printf "Pass\n"
+            else
+                example_failed_tests="${example_failed_tests} ${test_name}"
+                example_test_result=1
+                ((num_ex_failed++))
+                printf "Fail\n"
+            fi
+        fi
+    done
+
+    # Global tracking of success/fail for end report
+    if [[ ${num_ex_success} -gt 0 ]]
+    then
+        succeeded_tests="${succeeded_tests} ${example_succeeded_tests}"
+    fi
+    if [[ ${num_ex_failed} -gt 0 ]]
+    then
+        failed_tests="${failed_tests} ${example_failed_tests}"
+    fi
+
+    if [[ ${example_test_result} -eq 0 ]]
+    then
+        succeeded_examples="$succeeded_examples $example_name"
+        ((num_successful_examples++))
+    else
+        failed_examples="$failed_examples $example_name"
+        ((num_failed_examples++))
+    fi
+
+done
+num_examples=$((num_successful_examples + num_failed_examples))
 cd ${origin}
-echo "*** Done running examples!"
+date
+echo "*** Done running ${num_examples} examples!"
+if [[ $num_failed_examples -eq 0 ]]
+then
+    echo "*** No errors."
+else
+  echo "*** Errors detected in some examples."
+  echo "*** Failed examples ($num_failed_examples/$num_examples): $failed_examples"
+  echo "*** Failed tests: $failed_tests"
+fi
+
+if [[ $num_successful_examples -eq 0 ]]
+then
+    echo "*** No successful examples."
+else
+    echo "*** Some examples were fully successful."
+    echo "*** Successful examples: ($num_successful_examples/$num_examples): $succeeded_examples"
+fi
+
+if [[ -z "${succeeded_tests}" ]]; then
+    echo "*** No successful tests at all."
+else
+    echo "*** Some tests were successful."
+    echo "*** Successful tests: ${succeeded_tests}"
+fi
+
+# If any example testing failed, fail the runner script
+exit $num_failed_examples
