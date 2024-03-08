f27b985ad40e66609d1bd450bab3e1cc791dc12f
Matthias Diener <mdiener@illinois.edu>
2024-03-08 13:11:41 -0600

    scripts: move to FS-global / rank-local caches

--- scripts/delta-parallel-spawner.sh
+++ scripts/delta-parallel-spawner.sh
@@ -2,8 +2,8 @@
 #
 # Used to wrap the spawning of parallel mirgecom drivers on Delta.
 # unset CUDA_CACHE_DISABLE
-POCL_CACHE_ROOT=${POCL_CACHE_ROOT:-"/tmp/$USER/pocl-scratch"}
-XDG_CACHE_ROOT=${XDG_CACHE_HOME:-"/tmp/$USER/xdg-scratch"}
+POCL_CACHE_ROOT=${POCL_CACHE_ROOT:-"/projects/bbkf/$USER/pocl-scratch"}
+XDG_CACHE_ROOT=${XDG_CACHE_HOME:-"/projects/bbkf/$USER/xdg-scratch"}
 POCL_CACHE_DIR=${POCL_CACHE_DIR:-"${POCL_CACHE_ROOT}/rank$SLURM_PROCID"}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-"${XDG_CACHE_ROOT}/rank$SLURM_PROCID"}
 export POCL_CACHE_DIR
