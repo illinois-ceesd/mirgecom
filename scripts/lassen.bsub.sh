3ec68754d286f0c5d7dbabf704ba0da2c848a118
Matthias Diener <mdiener@illinois.edu>
2024-03-11 15:59:28 -0700

    enable CUDA cache

--- scripts/lassen.bsub.sh
+++ scripts/lassen.bsub.sh
@@ -32,13 +32,15 @@ export XDG_CACHE_HOME_ROOT="$(pwd)/xdg-scratch/rank"
 # (each rank needs its own POCL cache dir)
 export POCL_CACHE_DIR_ROOT="$(pwd)/pocl-scratch/rank"
 
+
+# Reenable CUDA cache
+export CUDA_CACHE_DISABLE=0
+export CUDA_CACHE_PATH_ROOT="$(pwd)/cuda-scratch/rank"
+
 # Print task allocation
 $jsrun_cmd js_task_info
 
 echo "----------------------------"
 
 # Run application
-# -O: switch on optimizations
-# POCL_CACHE_DIR=...: each rank needs its own POCL cache dir
-# XDG_CACHE_HOME=...: each rank needs its own Loopy/PyOpenCL cache dir
-$jsrun_cmd bash -c 'POCL_CACHE_DIR=$POCL_CACHE_DIR_ROOT$OMPI_COMM_WORLD_RANK XDG_CACHE_HOME=$XDG_CACHE_HOME_ROOT$OMPI_COMM_WORLD_RANK python -O -m mpi4py ./pulse-mpi.py'
+$jsrun_cmd bash -c 'CUDA_CACHE_PATH=$CUDA_CACHE_PATH_ROOT$OMPI_COMM_WORLD_RANK POCL_CACHE_DIR=$POCL_CACHE_DIR_ROOT$OMPI_COMM_WORLD_RANK XDG_CACHE_HOME=$XDG_CACHE_HOME_ROOT$OMPI_COMM_WORLD_RANK python -m mpi4py ../examples/pulse.py --lazy'
