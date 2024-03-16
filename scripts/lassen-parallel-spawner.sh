e5b7020293fece206570a209c461b9c4e01b094d
Michael T. Campbell <mtcampbe@illinois.edu>
2024-03-16 07:07:50 -0700

    make cache path control slightly more convenient

--- scripts/lassen-parallel-spawner.sh
+++ scripts/lassen-parallel-spawner.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #
 # Used to wrap the spawning of parallel mirgecom drivers on Lassen
-# unset CUDA_CACHE_DISABLE
-POCL_CACHE_ROOT=${POCL_CACHE_ROOT:-"$(pwd)/pocl-scratch"}
-XDG_CACHE_ROOT=${XDG_CACHE_HOME:-"$(pwd)/xdg-scratch"}
-POCL_CACHE_DIR=${POCL_CACHE_DIR:-"${POCL_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
-XDG_CACHE_HOME=${XDG_CACHE_HOME:-"${XDG_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
 
 # Reenable CUDA cache
+# unset CUDA_CACHE_DISABLE
 export CUDA_CACHE_DISABLE=0
-CUDA_CACHE_PATH_ROOT="$(pwd)/cuda-scratch"
-CUDA_CACHE_PATH=${CUDA_CACHE_PATH:-"${CUDA_CACHE_PATH_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
+
+# MIRGE env vars used to setup cache locations
+MIRGE_CACHE_ROOT=${MIRGE_CACHE_ROOT:-"$(pwd)"}
+POCL_CACHE_ROOT=${POCL_CACHE_ROOT:-"${MIRGE_CACHE_ROOT}/pocl-cache"}
+XDG_CACHE_ROOT=${XDG_CACHE_ROOT:-"${MIRGE_CACHE_ROOT}/xdg-cache"}
+CUDA_CACHE_ROOT=${CUDA_CACHE_ROOT:-"${MIRGE_CACHE_ROOT}/cuda-cache"}
+
+# These vars are used by pocl, pyopencl, loopy, and cuda for cache location
+POCL_CACHE_DIR=${POCL_CACHE_DIR:-"${POCL_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-"${XDG_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
+CUDA_CACHE_PATH=${CUDA_CACHE_PATH:-"${CUDA_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
 
 export POCL_CACHE_DIR
 export XDG_CACHE_HOME
