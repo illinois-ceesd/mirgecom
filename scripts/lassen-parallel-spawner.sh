69db9f25ece8bf6b441960476b38b2234b261201
Mike Campbell <mtcampbe@illinois.edu>
2024-03-23 08:46:23 -0500

    Give user control over cuda cache path, override system default.

--- scripts/lassen-parallel-spawner.sh
+++ scripts/lassen-parallel-spawner.sh
@@ -3,7 +3,6 @@
 # Used to wrap the spawning of parallel mirgecom drivers on Lassen
 
 # Reenable CUDA cache
-# unset CUDA_CACHE_DISABLE
 export CUDA_CACHE_DISABLE=0
 
 # MIRGE env vars used to setup cache locations
@@ -15,9 +14,10 @@ CUDA_CACHE_ROOT=${CUDA_CACHE_ROOT:-"${MIRGE_CACHE_ROOT}/cuda-cache"}
 # These vars are used by pocl, pyopencl, loopy, and cuda for cache location
 POCL_CACHE_DIR=${POCL_CACHE_DIR:-"${POCL_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-"${XDG_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
-# The system sets a default CUDA_CACHE_PATH which is node-local
+CUDA_CACHE_PATH=${CUDA_CACHE_DIR:-"${CUDA_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
+# The system sets a default CUDA_CACHE_PATH which is node-local :(
+# User still has full path control, but we discard the system default
 # CUDA_CACHE_PATH=${CUDA_CACHE_PATH:-"${CUDA_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"}
-CUDA_CACHE_PATH="${CUDA_CACHE_ROOT}/rank$OMPI_COMM_WORLD_RANK"
 
 export POCL_CACHE_DIR
 export XDG_CACHE_HOME
