90e5c537723794aa12fc2141bc54601acb4f0e20
Michael T. Campbell <mtcampbe@illinois.edu>
2024-09-09 15:29:28 -0700

    Update for tioga

--- scripts/tioga-parallel-spawner.sh
+++ scripts/tioga-parallel-spawner.sh
@@ -8,7 +8,10 @@ XDG_CACHE_ROOT=${XDG_CACHE_ROOT:-"${MIRGE_CACHE_ROOT}/xdg-cache"}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-"${XDG_CACHE_ROOT}/rank$FLUX_TASK_RANK"}
 
 export XDG_CACHE_HOME
-
-export ROCR_VISIBLE_DEVICES=$FLUX_TASK_LOCAL_ID
+echo "Before env customization (local,global) rank ($FLUX_TASK_LOCAL_ID,$FLUX_TASK_RANK)"
+env | grep ROCR_VISIBLE_DEVICES
+# export ROCR_VISIBLE_DEVICES=$FLUX_TASK_LOCAL_ID
+echo "After env customization (local,global) rank ($FLUX_TASK_LOCAL_ID,$FLUX_TASK_RANK)"
+env | grep ROCR_VISIBLE_DEVICES
 
 "$@"
