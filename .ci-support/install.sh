18c734351696a13a7eb1e54ae6b2e6c91010274d
Matthias Diener <mdiener@illinois.edu>
2025-05-08 11:57:57 -0500

    try mpich

--- .ci-support/install.sh
+++ .ci-support/install.sh
@@ -12,7 +12,7 @@ if [[ "$(uname)" = "Darwin" ]]; then
 else
     if ! command -v mpicc &> /dev/null ;then
         sudo apt-get update
-        sudo apt-get -y install libopenmpi-dev openmpi-bin
+        sudo apt-get -y install mpich
     fi
     if ! command -v octave &> /dev/null ;then
         sudo apt-get -y install octave
